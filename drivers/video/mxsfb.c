e27f5ad115641b19dd380b0526fce56a2e2bebb2
Hector Palacios <hector.palacios@digi.com>
2013-05-23 13:19:21 +0200

    video: mxsfb: fix settings for 18bit data bus and 32bpp
    For a selection of LCD data bus width of 18bits and a color
    mode of 32bpp, the driver was setting the color mapping to
    rgb666, which is wrong, as the color in memory realy has an
    rgb888 layout.
    
    This patch also removes the setting of flag CTRL_DF24 that
    makes the driver dimiss the upper 2 bits when handling 32/24bpp
    colors in a diplay with 18bit data bus width.
    This made true color images display wrong in such configurations.
    
    https://jira.digi.com/browse/DEL-482
    https://jira.digi.com/browse/DEL-297
    
    Signed-off-by: Hector Palacios <hector.palacios@digi.com>
    Reviewed-by: Juergen Beisert <jbe@pengutronix.de>

--- drivers/video/mxsfb.c
+++ drivers/video/mxsfb.c
@@ -309,9 +309,6 @@ static int mxsfb_check_var(struct fb_var_screeninfo *var,
 			break;
 		case STMLCDIF_16BIT:
 		case STMLCDIF_18BIT:
-			/* 24 bit to 18 bit mapping */
-			rgb = def_rgb666;
-			break;
 		case STMLCDIF_24BIT:
 			/* real 24 bit */
 			rgb = def_rgb888;
@@ -453,11 +450,6 @@ static int mxsfb_set_par(struct fb_info *fb_info)
 			return -EINVAL;
 		case STMLCDIF_16BIT:
 		case STMLCDIF_18BIT:
-			/* 24 bit to 18 bit mapping */
-			ctrl |= CTRL_DF24; /* ignore the upper 2 bits in
-					    *  each colour component
-					    */
-			break;
 		case STMLCDIF_24BIT:
 			/* real 24 bit */
 			break;
