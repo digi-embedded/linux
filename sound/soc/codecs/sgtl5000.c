a7c2ae697a032cea4cc3c62f30ff30aa609dff88
Alex Gonzalez <alex.gonzalez@digi.com>
2014-06-17 15:37:15 +0200

    soc: codecs: sgtl5000: ccimx6: Invert LRC polarity.
    This swaps the left and right channel so they sound correct on the
    "speaker_test -c2" application.
    
    This channel swapping does not happen on the sabresd default 3.10.17
    GA 140404 images, so the change is applied only to ccimx6 based platforms.
    
    Fixes https://jira.digi.com/browse/DEL-1827
    
    Signed-off-by: Alex Gonzalez <alex.gonzalez@digi.com>
    (cherry picked from commit 769a61a4e8a7c103533dc5e6d42257482167b6d3)
    Signed-off-by: Sesma, Iciar <Iciar.Sesma@digi.com>

--- sound/soc/codecs/sgtl5000.c
+++ sound/soc/codecs/sgtl5000.c
@@ -462,6 +462,8 @@ static int sgtl5000_set_dai_fmt(struct snd_soc_dai *codec_dai, unsigned int fmt)
 		break;
 	case SND_SOC_DAIFMT_I2S:
 		i2sctl |= SGTL5000_I2S_MODE_I2S_LJ;
+		if (of_machine_is_compatible("digi,ccimx6sbc"))
+			i2sctl |= SGTL5000_I2S_LRPOL;
 		break;
 	case SND_SOC_DAIFMT_RIGHT_J:
 		i2sctl |= SGTL5000_I2S_MODE_RJ;
