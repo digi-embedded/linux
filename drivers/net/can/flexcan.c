1b353e4ff827784b2795d7cee81c0054ea1f09c7
Mike Engel <Mike.Engel@digi.com>
2015-08-06 17:05:24 +0200

    driver: net: flexcan: Add support for the ccimx6sbc.
    Configure standby gpios in the Flexcan driver and created
    alias in DT.
    
    Signed-off-by: Alex Gonzalez <alex.gonzalez@digi.com>
    (cherry picked from commit 1ee92da2a934397d2a47479c3abe13d80ec18796)
    (cherry picked from commit 18f5ef88c65a6c700d9d1bee7084595416c60980)

--- drivers/net/can/flexcan.c
+++ drivers/net/can/flexcan.c
@@ -37,6 +37,7 @@
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
+#include <linux/of_gpio.h>
 #include <linux/platform_device.h>
 #include <linux/regmap.h>
 #include <linux/regulator/consumer.h>
@@ -274,6 +275,7 @@ struct flexcan_priv {
 	struct clk *clk_ipg;
 	struct clk *clk_per;
 	struct flexcan_platform_data *pdata;
+	int stby_gpio;
 	const struct flexcan_devtype_data *devtype_data;
 	struct regulator *reg_xceiver;
 	int id;
@@ -351,9 +353,14 @@ static inline void flexcan_exit_stop_mode(struct flexcan_priv *priv)
 
 static inline int flexcan_transceiver_enable(const struct flexcan_priv *priv)
 {
-	if (priv->pdata && priv->pdata->transceiver_switch) {
-		priv->pdata->transceiver_switch(1);
-		return 0;
+	if (priv->pdata){
+		if(gpio_is_valid(priv->stby_gpio))
+			gpio_set_value(priv->stby_gpio, 0);
+	} else {
+		if (priv->pdata->transceiver_switch) {
+			priv->pdata->transceiver_switch(1);
+			return 0;
+		}
 	}
 
 	if (!priv->reg_xceiver)
@@ -364,9 +371,14 @@ static inline int flexcan_transceiver_enable(const struct flexcan_priv *priv)
 
 static inline int flexcan_transceiver_disable(const struct flexcan_priv *priv)
 {
-	if (priv->pdata && priv->pdata->transceiver_switch) {
-		priv->pdata->transceiver_switch(0);
-		return 0;
+	if (priv->pdata){
+		if(gpio_is_valid(priv->stby_gpio))
+			gpio_set_value(priv->stby_gpio, 1);
+	} else {
+		if (priv->pdata->transceiver_switch) {
+			priv->pdata->transceiver_switch(0);
+			return 0;
+		}
 	}
 
 	if (!priv->reg_xceiver)
@@ -1247,6 +1259,25 @@ static const struct platform_device_id flexcan_id_table[] = {
 };
 MODULE_DEVICE_TABLE(platform, flexcan_id_table);
 
+static int flexcan_gpio_init(struct device_node *np , struct net_device *dev)
+{
+	int ret;
+	struct flexcan_priv *priv = netdev_priv(dev);
+
+	if (!np)
+		return -EINVAL;
+
+	priv->stby_gpio = of_get_named_gpio(np, "stby-gpios", 0);
+	if (!gpio_is_valid(priv->stby_gpio))
+		return -ENODEV;
+
+	if( (ret = gpio_request(priv->stby_gpio, "can_stby")) == 0)
+		gpio_direction_output(priv->stby_gpio, 1);
+	else
+		netdev_err(dev, "Could not configure standby pin.\n");
+	return ret;
+}
+
 static int flexcan_probe(struct platform_device *pdev)
 {
 	const struct of_device_id *of_id;
@@ -1327,6 +1358,7 @@ static int flexcan_probe(struct platform_device *pdev)
 	priv->pdata = dev_get_platdata(&pdev->dev);
 	priv->devtype_data = devtype_data;
 	priv->reg_xceiver = reg_xceiver;
+	flexcan_gpio_init(pdev->dev.of_node,dev);
 
 	netif_napi_add(dev, &priv->napi, flexcan_poll, FLEXCAN_NAPI_WEIGHT);
 
