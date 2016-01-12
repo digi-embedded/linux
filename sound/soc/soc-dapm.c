94b85de01a7ad112d5331ae07783e54dd4e1f675
Zidan Wang <zidan.wang@freescale.com>
2016-01-12 11:32:13 +0800

    MLK-12210 ASoC: soc-dapm: resource leak when the event is unknown
    resource leak when the event is unknown. Reported by coverity.
    
    Signed-off-by: Zidan Wang <zidan.wang@freescale.com>

--- sound/soc/soc-dapm.c
+++ sound/soc/soc-dapm.c
@@ -3308,7 +3308,8 @@ static int snd_soc_dai_link_event(struct snd_soc_dapm_widget *w,
 
 	default:
 		WARN(1, "Unknown event %d\n", event);
-		return -EINVAL;
+		ret = -EINVAL;
+		goto out;
 	}
 
 out:
