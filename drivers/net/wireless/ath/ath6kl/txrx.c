1a72d57a7e7eba760479385a0ffc6cbd130e4055
Mike Engel <Mike.Engel@digi.com>
2016-02-12 11:04:06 +0100

    ath6kl: Changed debug level
    This commit changes the debug level when Null data packages are
    received.
    
    Signed-off-by: Mike Engel <Mike.Engel@digi.com>
    
    https://jira.digi.com/browse/ADK4A-828
    (cherry picked from commit c578637692da9ff14d98fed29acdb5e55b8ea6a6)
    (cherry picked from commit e5e6ad9874f04c3a51fbcf73f7a884859fee6976)
    Signed-off-by: Arturo Buzarra <arturo.buzarra@digi.com>

--- drivers/net/wireless/ath/ath6kl/txrx.c
+++ drivers/net/wireless/ath/ath6kl/txrx.c
@@ -1389,7 +1389,8 @@ void ath6kl_rx(struct htc_target *target, struct htc_packet *packet)
 	if (vif->nw_type != AP_NETWORK &&
 	    ((packet->act_len < min_hdr_len) ||
 	     (packet->act_len > WMI_MAX_AMSDU_RX_DATA_FRAME_LENGTH))) {
-		ath6kl_info("frame len is too short or too long\n");
+		ath6kl_dbg(ATH6KL_DBG_WLAN_RX,
+			   "frame len is too short or too long\n");
 		vif->ndev->stats.rx_errors++;
 		vif->ndev->stats.rx_length_errors++;
 		dev_kfree_skb(skb);
