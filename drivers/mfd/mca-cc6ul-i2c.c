56466f25a002326750e06c3fee9adc14ff8e106b
Hector Palacios <hector.palacios@digi.com>
2016-04-12 13:02:08 +0200

    mfd: mca-cc6ul: added two registers for holding RTC year
    The MCA RTC driver uses a full year number (not just two digits) so lets
    use two registers COUNT_YEAR_L and COUNT_YEAR_H to hold the year as a
    16-bit number.
    
    Signed-off-by: Hector Palacios <hector.palacios@digi.com>
    
    https://jira.digi.com/browse/DEL-2419

--- drivers/mfd/mca-cc6ul-i2c.c
+++ drivers/mfd/mca-cc6ul-i2c.c
@@ -28,14 +28,14 @@ static const struct regmap_range mca_cc6ul_readable_ranges[] = {
 static const struct regmap_range mca_cc6ul_writeable_ranges[] = {
 	regmap_reg_range(MCA_CC6UL_IRQ_STATUS_0, MCA_CC6UL_IRQ_MASK_3),
 	regmap_reg_range(MCA_CC6UL_RTC_CONTROL, MCA_CC6UL_RTC_CONTROL),
-	regmap_reg_range(MCA_CC6UL_RTC_COUNT_YEAR, MCA_CC6UL_RTC_COUNT_SEC),
-	regmap_reg_range(MCA_CC6UL_RTC_ALARM_YEAR, MCA_CC6UL_RTC_ALARM_SEC),
+	regmap_reg_range(MCA_CC6UL_RTC_COUNT_YEAR_L, MCA_CC6UL_RTC_COUNT_SEC),
+	regmap_reg_range(MCA_CC6UL_RTC_ALARM_YEAR_L, MCA_CC6UL_RTC_ALARM_SEC),
 };
 
 static const struct regmap_range mca_cc6ul_volatile_ranges[] = {
 	/* Real volatile registers */
 	regmap_reg_range(MCA_CC6UL_IRQ_STATUS_0, MCA_CC6UL_IRQ_STATUS_3),
-	regmap_reg_range(MCA_CC6UL_RTC_COUNT_YEAR, MCA_CC6UL_RTC_COUNT_SEC),
+	regmap_reg_range(MCA_CC6UL_RTC_COUNT_YEAR_L, MCA_CC6UL_RTC_COUNT_SEC),
 
 	/*
 	 * Fake volatile registers.
@@ -51,7 +51,7 @@ static const struct regmap_range mca_cc6ul_volatile_ranges[] = {
 	regmap_reg_range(MCA_CC6UL_HWVER_L, MCA_CC6UL_UID_9),
 	regmap_reg_range(MCA_CC6UL_IRQ_MASK_0, MCA_CC6UL_IRQ_MASK_3),
 	regmap_reg_range(MCA_CC6UL_RTC_CONTROL, MCA_CC6UL_RTC_CONTROL),
-	regmap_reg_range(MCA_CC6UL_RTC_ALARM_YEAR, MCA_CC6UL_RTC_ALARM_SEC),
+	regmap_reg_range(MCA_CC6UL_RTC_ALARM_YEAR_L, MCA_CC6UL_RTC_ALARM_SEC),
 
 };
 
