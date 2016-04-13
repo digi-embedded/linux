f3516837facc6f2c71af3618894658c61f89695e
Hector Palacios <hector.palacios@digi.com>
2016-04-13 19:04:44 +0200

    rtc: rtc-mca-cc6ul: add RTC driver for ConnectCore 6UL MCA
    This RTC driver supports basic functionality:
     - set/get date
     - set/get alarm
     - alarm interrupt
    
    Signed-off-by: Hector Palacios <hector.palacios@digi.com>
    
    https://jira.digi.com/browse/DEL-2419

--- drivers/rtc/rtc-mca-cc6ul.c
+++ drivers/rtc/rtc-mca-cc6ul.c
@@ -1,5 +1,6 @@
-/* rtc-da9063.c - Real time clock device driver for DA9063
- * Copyright (C) 2013  Dialog Semiconductor Ltd.
+/* rtc-mca-cc6ul.c - Real time clock device driver for MCA on ConnectCore 6UL
+ * Copyright (C) 2016  Digi International
+ * Copyright (c) Dialog Semiconductor Ltd.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -25,135 +26,134 @@
 #include <linux/rtc.h>
 #include <linux/slab.h>
 #include <linux/delay.h>
-#include <linux/mfd/da9063/registers.h>
-#include <linux/mfd/da9063/core.h>
+#include <linux/mfd/mca-cc6ul/registers.h>
+#include <linux/mfd/mca-cc6ul/core.h>
 #include <linux/of.h>
+#include <linux/of_irq.h>
 #include <linux/regmap.h>
 
-#define CLOCK_DATA_LEN    (DA9063_REG_COUNT_Y    - DA9063_REG_COUNT_S     + 1)
-#define ALARM_AD_DATA_LEN (DA9063_AD_REG_ALARM_Y - DA9063_AD_REG_ALARM_MI + 1)
-#define ALARM_DATA_LEN    (DA9063_REG_ALARM_Y    - DA9063_REG_ALARM_S     + 1)
+#define CLOCK_DATA_LEN	(MCA_CC6UL_RTC_COUNT_SEC - MCA_CC6UL_RTC_COUNT_YEAR_L + 1)
+#define ALARM_DATA_LEN	(MCA_CC6UL_RTC_ALARM_SEC - MCA_CC6UL_RTC_ALARM_YEAR_L + 1)
 
 enum {
-	DATA_SEC = 0,
-	DATA_MIN,
-	DATA_HOUR,
-	DATA_DAY,
+	DATA_YEAR_L,
+	DATA_YEAR_H,
 	DATA_MONTH,
-	DATA_YEAR,
+	DATA_DAY,
+	DATA_HOUR,
+	DATA_MIN,
+	DATA_SEC,
 };
 
-struct da9063_rtc {
-	struct rtc_device	*rtc_dev;
-	struct da9063		*hw;
-	int			irq_alarm;
-	unsigned int		chip_revision;
+struct mca_cc6ul_rtc {
+	struct rtc_device *rtc_dev;
+	struct mca_cc6ul *mca;
+	int irq_alarm;
 };
 
-static void da9063_data_to_tm(u8 *data, struct rtc_time *tm)
+static void mca_cc6ul_data_to_tm(u8 *data, struct rtc_time *tm)
 {
-	tm->tm_sec  = (data[DATA_SEC]   & DA9063_COUNT_SEC_MASK  );
-	tm->tm_min  = (data[DATA_MIN]   & DA9063_COUNT_MIN_MASK  );
-	tm->tm_hour = (data[DATA_HOUR]  & DA9063_COUNT_HOUR_MASK );
-	tm->tm_mday = (data[DATA_DAY]   & DA9063_COUNT_DAY_MASK  );
-	/* conversion is from da9063 to real is month-1 and year-100 */
-	tm->tm_mon  = (data[DATA_MONTH] & DA9063_COUNT_MONTH_MASK) - 1;
-	tm->tm_year = (data[DATA_YEAR]  & DA9063_COUNT_YEAR_MASK ) + 100;
+	/* conversion from MCA RTC to struct time is month-1 and year-1900 */
+	tm->tm_year = (((data[DATA_YEAR_H] &
+			 MCA_CC6UL_RTC_COUNT_YEAR_H_MASK) << 8) |
+		       (data[DATA_YEAR_L] & MCA_CC6UL_RTC_COUNT_YEAR_L_MASK)) -
+		       1900;
+	tm->tm_mon  = (data[DATA_MONTH] & MCA_CC6UL_RTC_COUNT_MONTH_MASK) - 1;
+	tm->tm_mday = (data[DATA_DAY]   & MCA_CC6UL_RTC_COUNT_DAY_MASK);
+	tm->tm_hour = (data[DATA_HOUR]  & MCA_CC6UL_RTC_COUNT_HOUR_MASK);
+	tm->tm_min  = (data[DATA_MIN]   & MCA_CC6UL_RTC_COUNT_MIN_MASK);
+	tm->tm_sec  = (data[DATA_SEC]   & MCA_CC6UL_RTC_COUNT_SEC_MASK);
 }
 
-static void da9063_tm_to_data(struct rtc_time *tm, u8 *data)
+static void mca_cc6ul_tm_to_data(struct rtc_time *tm, u8 *data)
 {
-	data[DATA_SEC]   &= ~DA9063_COUNT_SEC_MASK;
-	data[DATA_SEC]   |= tm->tm_sec & DA9063_COUNT_SEC_MASK;
+	/* conversion from struct time to MCA RTC is year+1900 */
+	data[DATA_YEAR_L]  &= (u8)~MCA_CC6UL_RTC_COUNT_YEAR_L_MASK;
+	data[DATA_YEAR_H]  &= (u8)~MCA_CC6UL_RTC_COUNT_YEAR_H_MASK;
+	data[DATA_YEAR_L]  |= (tm->tm_year + 1900) &
+			      MCA_CC6UL_RTC_COUNT_YEAR_L_MASK;
+	data[DATA_YEAR_H]  |= ((tm->tm_year + 1900) >> 8) &
+			       MCA_CC6UL_RTC_COUNT_YEAR_H_MASK;
 
-	data[DATA_MIN]   &= ~DA9063_COUNT_MIN_MASK;
-	data[DATA_MIN]   |= tm->tm_min & DA9063_COUNT_MIN_MASK;
+	/* conversion from struct time to MCA RTC is month+1 */
+	data[DATA_MONTH] &= ~MCA_CC6UL_RTC_COUNT_MONTH_MASK;
+	data[DATA_MONTH] |= (tm->tm_mon + 1) & MCA_CC6UL_RTC_COUNT_MONTH_MASK;
 
-	data[DATA_HOUR]  &= ~DA9063_COUNT_HOUR_MASK;
-	data[DATA_HOUR]  |= tm->tm_hour & DA9063_COUNT_HOUR_MASK;
+	data[DATA_DAY]   &= ~MCA_CC6UL_RTC_COUNT_DAY_MASK;
+	data[DATA_DAY]   |= tm->tm_mday & MCA_CC6UL_RTC_COUNT_DAY_MASK;
 
-	data[DATA_DAY]   &= ~DA9063_COUNT_DAY_MASK;
-	data[DATA_DAY]   |= tm->tm_mday & DA9063_COUNT_DAY_MASK;
+	data[DATA_HOUR]  &= ~MCA_CC6UL_RTC_COUNT_HOUR_MASK;
+	data[DATA_HOUR]  |= tm->tm_hour & MCA_CC6UL_RTC_COUNT_HOUR_MASK;
 
-	/* conversion is from real to da9063 is month+1  */
-	data[DATA_MONTH] &= ~DA9063_COUNT_MONTH_MASK;
-	data[DATA_MONTH] |= (tm->tm_mon+1) & DA9063_COUNT_MONTH_MASK;
+	data[DATA_MIN]   &= ~MCA_CC6UL_RTC_COUNT_MIN_MASK;
+	data[DATA_MIN]   |= tm->tm_min & MCA_CC6UL_RTC_COUNT_MIN_MASK;
 
-	/* conversion is from real to da9063 is year-100 */
-	data[DATA_YEAR]  &= ~DA9063_COUNT_YEAR_MASK;
-	data[DATA_YEAR]  |= (tm->tm_year-100) & DA9063_COUNT_YEAR_MASK;
+	data[DATA_SEC]   &= ~MCA_CC6UL_RTC_COUNT_SEC_MASK;
+	data[DATA_SEC]   |= tm->tm_sec & MCA_CC6UL_RTC_COUNT_SEC_MASK;
 }
 
-static int da9063_rtc_stop_alarm(struct device *dev)
+static int mca_cc6ul_rtc_stop_alarm(struct device *dev)
 {
-	struct da9063_rtc *rtc = dev_get_drvdata(dev);
-	int ret = 0;
+	struct mca_cc6ul_rtc *rtc = dev_get_drvdata(dev);
 
-	switch( rtc->chip_revision ) {
-	case DA9063_AD_REVISION:
-		ret = regmap_update_bits(rtc->hw->regmap, DA9063_AD_REG_ALARM_Y,
-					 DA9063_ALARM_ON, 0);
-		break;
-	case DA9063_BB_REVISION:
-	default:
-		ret = regmap_update_bits(rtc->hw->regmap, DA9063_REG_ALARM_Y,
-					 DA9063_ALARM_ON, 0);
-	}
-
-	return ret;
+#ifdef MCA_CC6UL_CRC
+	// TODO: add wrapper for regmap_update_bits with CRC
+#else
+	return regmap_update_bits(rtc->mca->regmap, MCA_CC6UL_RTC_CONTROL,
+				  MCA_CC6UL_RTC_ALARM_EN, 0);
+#endif
 }
 
-static int da9063_rtc_start_alarm(struct device *dev)
+static int mca_cc6ul_rtc_start_alarm(struct device *dev)
 {
-	struct da9063_rtc *rtc = dev_get_drvdata(dev);
-	int ret = 0;
+	struct mca_cc6ul_rtc *rtc = dev_get_drvdata(dev);
 
-	switch( rtc->chip_revision ) {
-	case DA9063_AD_REVISION:
-		ret = regmap_update_bits(rtc->hw->regmap, DA9063_AD_REG_ALARM_Y,
-					 DA9063_ALARM_ON, DA9063_ALARM_ON);
-		break;
-	case DA9063_BB_REVISION:
-	default:
-		ret = regmap_update_bits(rtc->hw->regmap, DA9063_REG_ALARM_Y,
-					 DA9063_ALARM_ON, DA9063_ALARM_ON);
-	}
-
-	return ret;
+#ifdef MCA_CC6UL_CRC
+	// TODO: add wrapper for regmap_update_bits with CRC
+#else
+	return regmap_update_bits(rtc->mca->regmap, MCA_CC6UL_RTC_CONTROL,
+				  MCA_CC6UL_RTC_ALARM_EN,
+				  MCA_CC6UL_RTC_ALARM_EN);
+#endif
 }
 
-static int da9063_rtc_read_time(struct device *dev, struct rtc_time *tm)
+static int mca_cc6ul_rtc_read_time(struct device *dev, struct rtc_time *tm)
 {
-	struct da9063_rtc *rtc = dev_get_drvdata(dev);
+	struct mca_cc6ul_rtc *rtc = dev_get_drvdata(dev);
 	u8 data[CLOCK_DATA_LEN] = { [0 ... (CLOCK_DATA_LEN - 1)] = 0 };
 	int ret;
 
-	ret = regmap_bulk_read(rtc->hw->regmap,
-				DA9063_REG_COUNT_S, data, CLOCK_DATA_LEN);
+#ifdef MCA_CC6UL_CRC
+	ret = mca_cc6ul_read_block(rtc->mca, MCA_CC6UL_RTC_COUNT_YEAR_L, data
+				   CLOCK_DATA_LEN);
+#else
+	ret = regmap_bulk_read(rtc->mca->regmap, MCA_CC6UL_RTC_COUNT_YEAR_L,
+			       data, CLOCK_DATA_LEN);
+#endif
 	if (ret < 0) {
 		dev_err(dev, "Failed to read RTC time data: %d\n", ret);
 		return ret;
 	}
 
-	if (!(data[DATA_SEC] & DA9063_RTC_READ)) {
-		dev_dbg(dev, "RTC not yet ready to be read by the host\n");
-		return -EINVAL;
-	}
-
-	da9063_data_to_tm(data, tm);
+	mca_cc6ul_data_to_tm(data, tm);
 	return rtc_valid_tm(tm);
 }
 
-static int da9063_rtc_set_time(struct device *dev, struct rtc_time *tm)
+static int mca_cc6ul_rtc_set_time(struct device *dev, struct rtc_time *tm)
 {
-	struct da9063_rtc *rtc = dev_get_drvdata(dev);
+	struct mca_cc6ul_rtc *rtc = dev_get_drvdata(dev);
 	u8 data[CLOCK_DATA_LEN] = { [0 ... (CLOCK_DATA_LEN - 1)] = 0 };
 	int ret;
 
-	da9063_tm_to_data(tm, data);
+	mca_cc6ul_tm_to_data(tm, data);
 
-	ret = regmap_bulk_write(rtc->hw->regmap,
-				 DA9063_REG_COUNT_S, data, CLOCK_DATA_LEN);
+#ifdef MCA_CC6UL_CRC
+	ret = mca_cc6ul_write_block(rtc->mca, MCA_CC6UL_RTC_COUNT_YEAR_L, data,
+				    CLOCK_DATA_LEN);
+#else
+	ret = regmap_bulk_write(rtc->mca->regmap, MCA_CC6UL_RTC_COUNT_YEAR_L,
+				data, CLOCK_DATA_LEN);
+#endif
 	if (ret < 0) {
 		dev_err(dev, "Failed to set RTC time data: %d\n", ret);
 		return ret;
@@ -162,182 +162,113 @@ static int da9063_rtc_set_time(struct device *dev, struct rtc_time *tm)
 	return ret;
 }
 
-static int da9063_rtc_read_alarm(struct device *dev, struct rtc_wkalrm *alrm)
+static int mca_cc6ul_rtc_read_alarm(struct device *dev, struct rtc_wkalrm *alrm)
 {
-	struct da9063_rtc *rtc = dev_get_drvdata(dev);
+	struct mca_cc6ul_rtc *rtc = dev_get_drvdata(dev);
 	u8 data[CLOCK_DATA_LEN] = { [0 ... (CLOCK_DATA_LEN - 1)] = 0 };
 	int ret;
 	unsigned int val;
 
-	switch( rtc->chip_revision ) {
-	case DA9063_AD_REVISION:
-		ret = regmap_bulk_read(rtc->hw->regmap, DA9063_AD_REG_ALARM_MI,
-				       &data[DATA_MIN], ALARM_AD_DATA_LEN);
-		break;
-	case DA9063_BB_REVISION:
-	default:
-		ret = regmap_bulk_read(rtc->hw->regmap, DA9063_REG_ALARM_S,
-				       &data[DATA_SEC], ALARM_DATA_LEN);
-	}
-
+#ifdef MCA_CC6UL_CRC
+	ret = mca_cc6ul_read_block(rtc->mca, MCA_CC6UL_RTC_ALARM_YEAR_L, data,
+				   ALARM_DATA_LEN);
+#else
+	ret = regmap_bulk_read(rtc->mca->regmap, MCA_CC6UL_RTC_ALARM_YEAR_L,
+			       data, ALARM_DATA_LEN);
+#endif
 	if (ret < 0)
 		return ret;
 
-	da9063_data_to_tm(data, &alrm->time);
-	alrm->enabled = !!(data[DATA_YEAR] & DA9063_ALARM_ON);
+	mca_cc6ul_data_to_tm(data, &alrm->time);
 
-	/* If RTC event is not processed yet, indicate pending */
-	ret = regmap_read(rtc->hw->regmap, DA9063_REG_EVENT_A, &val);
+	/* Enable status */
+#ifdef MCA_CC6UL_CRC
+	ret = mca_cc6ul_read_block(rtc->mca, MCA_CC6UL_RTC_CONTROL,
+				   (u8 *)&val, 1);
+#else
+	ret = regmap_read(rtc->mca->regmap, MCA_CC6UL_RTC_CONTROL, &val);
+#endif
 	if (ret < 0)
 		return ret;
 
-	if (ret & (DA9063_E_ALARM))
-		alrm->pending = 1;
-	else
-		alrm->pending = 0;
+	/* Pending status */
+#ifdef MCA_CC6UL_CRC
+	ret = mca_cc6ul_read_block(rtc->mca, MCA_CC6UL_IRQ_STATUS_0,
+				   (u8 *)&val, 1);
+#else
+	ret = regmap_read(rtc->mca->regmap, MCA_CC6UL_IRQ_STATUS_0, &val);
+#endif
+	if (ret < 0)
+		return ret;
+	alrm->pending = (val & MCA_CC6UL_M_RTC_ALARM) ? 1 : 0;
 
 	return 0;
 }
 
-static int da9063_rtc_set_alarm(struct device *dev, struct rtc_wkalrm *alrm)
+static int mca_cc6ul_rtc_set_alarm(struct device *dev, struct rtc_wkalrm *alrm)
 {
-	struct da9063_rtc *rtc = dev_get_drvdata(dev);
+	struct mca_cc6ul_rtc *rtc = dev_get_drvdata(dev);
 	u8 data[CLOCK_DATA_LEN] = { [0 ... (CLOCK_DATA_LEN - 1)] = 0 };
 	int ret;
 
-	da9063_tm_to_data(&alrm->time, data);
+	mca_cc6ul_tm_to_data(&alrm->time, data);
 
-	ret = da9063_rtc_stop_alarm(dev);
-	if (ret < 0) {
-		dev_err(dev, "Failed to stop alarm: %d\n", ret);
-		return ret;
-	}
-
-	switch( rtc->chip_revision ) {
-	case DA9063_AD_REVISION:
-		ret = regmap_bulk_write(rtc->hw->regmap, DA9063_AD_REG_ALARM_MI,
-				       &data[DATA_MIN], ALARM_AD_DATA_LEN);
-		break;
-	case DA9063_BB_REVISION:
-	default:
-		ret = regmap_bulk_write(rtc->hw->regmap, DA9063_REG_ALARM_S,
-				       &data[DATA_SEC], ALARM_DATA_LEN);
-	}
-
-	if (alrm->enabled) {
-		ret = da9063_rtc_start_alarm(dev);
-		if (ret < 0) {
-			dev_err(dev, "Failed to start alarm: %d\n", ret);
-			return ret;
-		}
-	}
+#ifdef MCA_CC6UL_CRC
+	ret = mca_cc6ul_write_block(rtc->mca, MCA_CC6UL_RTC_ALARM_YEAR_L,
+				    data, ALARM_DATA_LEN);
+#else
+	ret = regmap_bulk_write(rtc->mca->regmap, MCA_CC6UL_RTC_ALARM_YEAR_L,
+				data, ALARM_DATA_LEN);
+#endif
 
 	return ret;
 }
 
-static int da9063_rtc_alarm_irq_enable(struct device *dev, unsigned int enabled)
+static int mca_cc6ul_rtc_alarm_irq_enable(struct device *dev,
+					  unsigned int enabled)
 {
-
 	if (enabled)
-		return da9063_rtc_start_alarm(dev);
+		return mca_cc6ul_rtc_start_alarm(dev);
 	else
-		return da9063_rtc_stop_alarm(dev);
+		return mca_cc6ul_rtc_stop_alarm(dev);
 }
 
-/* On alarm interrupt, start to count ticks to enable seconds precision
-   (if alarm seconds != 0). */
-static irqreturn_t da9063_alarm_event(int irq, void *data)
+static irqreturn_t mca_cc6ul_alarm_event(int irq, void *data)
 {
-	struct da9063_rtc *rtc = data;
-
-	switch( rtc->chip_revision ) {
-	case DA9063_AD_REVISION:
-		regmap_update_bits(rtc->hw->regmap, DA9063_AD_REG_ALARM_Y,
-				      DA9063_ALARM_ON, 0);
-		break;
-	case DA9063_BB_REVISION:
-	default:
-		regmap_update_bits(rtc->hw->regmap, DA9063_REG_ALARM_Y,
-				      DA9063_ALARM_ON, 0);
-	}
+	struct mca_cc6ul_rtc *rtc = data;
 
 	rtc_update_irq(rtc->rtc_dev, 1, RTC_IRQF | RTC_AF);
 
 	return IRQ_HANDLED;
 }
 
-static const struct rtc_class_ops da9063_rtc_ops = {
-	.read_time = da9063_rtc_read_time,
-	.set_time = da9063_rtc_set_time,
-	.read_alarm = da9063_rtc_read_alarm,
-	.set_alarm = da9063_rtc_set_alarm,
-	.alarm_irq_enable = da9063_rtc_alarm_irq_enable,
+static const struct rtc_class_ops mca_cc6ul_rtc_ops = {
+	.read_time = mca_cc6ul_rtc_read_time,
+	.set_time = mca_cc6ul_rtc_set_time,
+	.read_alarm = mca_cc6ul_rtc_read_alarm,
+	.set_alarm = mca_cc6ul_rtc_set_alarm,
+	.alarm_irq_enable = mca_cc6ul_rtc_alarm_irq_enable,
 };
 
-static int da9063_rtc_probe(struct platform_device *pdev)
+static int mca_cc6ul_rtc_probe(struct platform_device *pdev)
 {
-	struct da9063 *da9063 = dev_get_drvdata(pdev->dev.parent);
-	struct da9063_rtc *rtc;
+	struct mca_cc6ul *mca = dev_get_drvdata(pdev->dev.parent);
+	struct mca_cc6ul_rtc *rtc;
+//	struct device_node *np;
 	int ret;
 
-	if (!da9063 || !da9063->dev->parent->of_node) {
+	if (!mca || !mca->dev->parent->of_node)
 		return -EPROBE_DEFER;
-        }
 
 	/* Enable RTC hardware */
-	ret = regmap_update_bits(da9063->regmap, DA9063_REG_CONTROL_E,
-				 DA9063_RTC_EN, DA9063_RTC_EN);
+#ifdef MCA_CC6UL_CRC
+	// TODO: add wrapper for regmap_update_bits with CRC
+#else
+	ret = regmap_update_bits(mca->regmap, MCA_CC6UL_RTC_CONTROL,
+				 MCA_CC6UL_RTC_EN, MCA_CC6UL_RTC_EN);
+#endif
 	if (ret < 0) {
 		dev_err(&pdev->dev, "Failed to enable RTC.\n");
-		return ret;
-	}
-
-	ret = regmap_update_bits(da9063->regmap, DA9063_REG_EN_32K,
-				 DA9063_CRYSTAL, DA9063_CRYSTAL);
-	if (ret < 0) {
-		dev_err(&pdev->dev, "Failed to run 32 KHz OSC.\n");
-		return ret;
-	}
-
-	/* Set alarm reason (alarm only, no tick) */
-	switch( da9063->revision ) {
-	case DA9063_AD_REVISION:
-		ret = regmap_update_bits(da9063->regmap, DA9063_AD_REG_ALARM_MI,
-					 DA9063_ALARM_STATUS_TICK |
-					 DA9063_ALARM_STATUS_ALARM, 0);
-		ret = regmap_update_bits(da9063->regmap, DA9063_AD_REG_ALARM_MI,
-					    DA9063_ALARM_STATUS_ALARM,
-					    DA9063_ALARM_STATUS_ALARM);
-		break;
-	case DA9063_BB_REVISION:
-	default:
-		ret = regmap_update_bits(da9063->regmap, DA9063_REG_ALARM_S,
-					    DA9063_ALARM_STATUS_TICK |
-					    DA9063_ALARM_STATUS_ALARM, 0);
-		ret = regmap_update_bits(da9063->regmap, DA9063_REG_ALARM_S,
-					    DA9063_ALARM_STATUS_ALARM,
-					    DA9063_ALARM_STATUS_ALARM);
-	}
-
-	if (ret < 0) {
-		dev_err(&pdev->dev, "Failed to access RTC alarm register.\n");
-		return ret;
-	}
-
-	/* Make sure that ticks are disabled. */
-	switch( da9063->revision ) {
-	case DA9063_AD_REVISION:
-		ret = regmap_update_bits(da9063->regmap, DA9063_AD_REG_ALARM_Y,
-					    DA9063_TICK_ON, 0);
-		break;
-	case DA9063_BB_REVISION:
-	default:
-		ret = regmap_update_bits(da9063->regmap, DA9063_REG_ALARM_Y,
-					    DA9063_TICK_ON, 0);
-	}
-
-	if (ret < 0) {
-		dev_err(&pdev->dev, "Failed to access RTC alarm register.\n");
 		return ret;
 	}
 
@@ -347,12 +278,10 @@ static int da9063_rtc_probe(struct platform_device *pdev)
 		return -ENOMEM;
 
 	platform_set_drvdata(pdev, rtc);
-
 	device_init_wakeup(&pdev->dev, 1);
-	rtc->chip_revision = da9063->revision;
-	rtc->hw = da9063;
-	rtc->rtc_dev = rtc_device_register(DA9063_DRVNAME_RTC, &pdev->dev,
-					   &da9063_rtc_ops, THIS_MODULE);
+	rtc->mca = mca;
+	rtc->rtc_dev = rtc_device_register(MCA_CC6UL_DRVNAME_RTC, &pdev->dev,
+					   &mca_cc6ul_rtc_ops, THIS_MODULE);
 	if (IS_ERR(rtc->rtc_dev)) {
 		dev_err(&pdev->dev, "Failed to register RTC device: %ld\n",
 			PTR_ERR(rtc->rtc_dev));
@@ -360,13 +289,15 @@ static int da9063_rtc_probe(struct platform_device *pdev)
 	}
 	rtc->rtc_dev->dev.of_node = pdev->dev.of_node;
 
-	/* Register interrupts. Complain on errors but let device
-	   to be registered at least for date/time. */
+	/*
+	 * Register interrupts. Complain on errors but let device
+	 * to be registered at least for date/time.
+	 */
 	rtc->irq_alarm = platform_get_irq_byname(pdev, "ALARM");
-	ret = request_threaded_irq(rtc->irq_alarm, NULL, da9063_alarm_event,
+	ret = request_threaded_irq(rtc->irq_alarm, NULL, mca_cc6ul_alarm_event,
 				IRQF_TRIGGER_LOW | IRQF_ONESHOT, "ALARM", rtc);
 	if (ret) {
-		dev_err(&pdev->dev, "Failed to request ALARM IRQ.\n");
+		dev_err(&pdev->dev, "Failed to request ALARM IRQ. (%d)\n", rtc->irq_alarm);
 		rtc->irq_alarm = -ENXIO;
 		return 0;
 	}
@@ -374,9 +305,9 @@ static int da9063_rtc_probe(struct platform_device *pdev)
 	return 0;
 }
 
-static int da9063_rtc_remove(struct platform_device *pdev)
+static int mca_cc6ul_rtc_remove(struct platform_device *pdev)
 {
-	struct da9063_rtc *rtc = platform_get_drvdata(pdev);
+	struct mca_cc6ul_rtc *rtc = platform_get_drvdata(pdev);
 
 	if (rtc->irq_alarm >= 0)
 		free_irq(rtc->irq_alarm, rtc);
@@ -386,39 +317,39 @@ static int da9063_rtc_remove(struct platform_device *pdev)
 }
 
 #ifdef CONFIG_OF
-static const struct of_device_id dialog_dt_ids[] = {
-        { .compatible = "dlg,da9063-rtc", },
+static const struct of_device_id mca_cc6ul_rtc_dt_ids[] = {
+        { .compatible = "digi,mca-cc6ul-rtc", },
         { /* sentinel */ }
 };
-MODULE_DEVICE_TABLE(of, dialog_dt_ids);
+MODULE_DEVICE_TABLE(of, mca_cc6ul_rtc_dt_ids);
 #endif
 
-static struct platform_driver da9063_rtc_driver = {
-	.probe		= da9063_rtc_probe,
-	.remove		= da9063_rtc_remove,
+static struct platform_driver mca_cc6ul_rtc_driver = {
+	.probe		= mca_cc6ul_rtc_probe,
+	.remove		= mca_cc6ul_rtc_remove,
 	.driver		= {
-		.name	= DA9063_DRVNAME_RTC,
+		.name	= MCA_CC6UL_DRVNAME_RTC,
 		.owner	= THIS_MODULE,
 #ifdef CONFIG_OF
-                .of_match_table = dialog_dt_ids,
+                .of_match_table = mca_cc6ul_rtc_dt_ids,
 #endif
 	},
 };
 
-static int __init da9063_rtc_init(void)
+static int __init mca_cc6ul_rtc_init(void)
 {
-	return platform_driver_register(&da9063_rtc_driver);
+	return platform_driver_register(&mca_cc6ul_rtc_driver);
 }
-module_init(da9063_rtc_init);
+module_init(mca_cc6ul_rtc_init);
 
-static void __exit da9063_rtc_exit(void)
+static void __exit mca_cc6ul_rtc_exit(void)
 {
-	platform_driver_unregister(&da9063_rtc_driver);
+	platform_driver_unregister(&mca_cc6ul_rtc_driver);
 }
-module_exit(da9063_rtc_exit);
+module_exit(mca_cc6ul_rtc_exit);
 
 /* Module information */
-MODULE_AUTHOR("S Twiss <stwiss.opensource@diasemi.com>");
-MODULE_DESCRIPTION("Real time clock device driver for Dialog DA9063");
+MODULE_AUTHOR("Digi International Inc.");
+MODULE_DESCRIPTION("Real time clock device driver for MCA of ConnectCore 6UL");
 MODULE_LICENSE("GPL v2");
-MODULE_ALIAS("platform:" DA9063_DRVNAME_RTC);
+MODULE_ALIAS("platform:" MCA_CC6UL_DRVNAME_RTC);
