bfdf4fc6782dfe2e9246c354399fbdeaa5371bd3
Hector Palacios <hector.palacios@digi.com>
2016-09-09 11:11:35 +0200

    ASoC: max98088: add prescaler support
    The codec datasheet uses a prescaler (PSCLK) at register 0x10[5:4] as
    follows (binary B5,B4):
    
     01 = 10MHz <= MCLK <= 20MHz (PCLK = MCLK)
     10 = 20MHz <= MCLK <= 40MHz (PCLK = MCLK/2)
     11 = 40MHz <= MCLK <= 60MHz (PCLK = MCLK/4)
    
    NI must be calculated taking the prescaler into account.
    
    Signed-off-by: Hector Palacios <hector.palacios@digi.com>

--- sound/soc/codecs/max98088.c
+++ sound/soc/codecs/max98088.c
@@ -43,6 +43,7 @@ struct max98088_priv {
 	enum max98088_type devtype;
 	struct max98088_pdata *pdata;
 	unsigned int sysclk;
+	unsigned int prescaler;
 	struct max98088_cdata dai[2];
 	int eq_textcnt;
 	const char **eq_texts;
@@ -1005,7 +1006,8 @@ static int max98088_dai1_hw_params(struct snd_pcm_substream *substream,
                }
                ni = 65536ULL * (rate < 50000 ? 96ULL : 48ULL)
                                * (unsigned long long int)rate;
-               do_div(ni, (unsigned long long int)max98088->sysclk);
+		do_div(ni, (unsigned long long int)(max98088->sysclk /
+		       max98088->prescaler));
                snd_soc_write(codec, M98088_REG_12_DAI1_CLKCFG_HI,
                        (ni >> 8) & 0x7F);
                snd_soc_write(codec, M98088_REG_13_DAI1_CLKCFG_LO,
@@ -1072,7 +1074,8 @@ static int max98088_dai2_hw_params(struct snd_pcm_substream *substream,
                }
                ni = 65536ULL * (rate < 50000 ? 96ULL : 48ULL)
                                * (unsigned long long int)rate;
-               do_div(ni, (unsigned long long int)max98088->sysclk);
+		do_div(ni, (unsigned long long int)(max98088->sysclk /
+		       max98088->prescaler));
                snd_soc_write(codec, M98088_REG_1A_DAI2_CLKCFG_HI,
                        (ni >> 8) & 0x7F);
                snd_soc_write(codec, M98088_REG_1B_DAI2_CLKCFG_LO,
@@ -1098,19 +1101,26 @@ static int max98088_dai_set_sysclk(struct snd_soc_dai *dai,
 {
        struct snd_soc_codec *codec = dai->codec;
        struct max98088_priv *max98088 = snd_soc_codec_get_drvdata(codec);
+	unsigned int prescaler = 0;
 
        /* Requested clock frequency is already setup */
        if (freq == max98088->sysclk)
                return 0;
 
-       /* Setup clocks for slave mode, and using the PLL
-        * PSCLK = 0x01 (when master clk is 10MHz to 20MHz)
-        *         0x02 (when master clk is 20MHz to 30MHz)..
-        */
+	/* Setup clocks for slave mode, and using the PLL
+	 * PSCLK = 0x01 (when master clk is 10MHz to 20MHz)
+	 *         0x02 (when master clk is 20MHz to 40MHz)..
+	 *         0x04 (when master clk is 40MHz to 60MHz)..
+	 */
        if ((freq >= 10000000) && (freq < 20000000)) {
                snd_soc_write(codec, M98088_REG_10_SYS_CLK, 0x10);
-       } else if ((freq >= 20000000) && (freq < 30000000)) {
-               snd_soc_write(codec, M98088_REG_10_SYS_CLK, 0x20);
+		prescaler = 1;
+	} else if ((freq >= 20000000) && (freq < 40000000)) {
+		snd_soc_write(codec, M98088_REG_10_SYS_CLK, 0x20);
+		prescaler = 2;
+	} else if ((freq >= 40000000) && (freq < 60000000)) {
+		snd_soc_write(codec, M98088_REG_10_SYS_CLK, 0x30);
+		prescaler = 4;
        } else {
                dev_err(codec->dev, "Invalid master clock frequency\n");
                return -EINVAL;
@@ -1123,9 +1133,12 @@ static int max98088_dai_set_sysclk(struct snd_soc_dai *dai,
                        M98088_SHDNRUN, M98088_SHDNRUN);
        }
 
-       dev_dbg(dai->dev, "Clock source is %d at %uHz\n", clk_id, freq);
+	dev_dbg(dai->dev, "Clock source is %d, freq=%uHz, prescaler:%u\n",
+		clk_id, freq, prescaler);
 
        max98088->sysclk = freq;
+	max98088->prescaler = prescaler;
+
        return 0;
 }
 
@@ -1639,6 +1652,7 @@ static int max98088_probe(struct snd_soc_codec *codec)
        /* initialize private data */
 
        max98088->sysclk = (unsigned)-1;
+	max98088->prescaler = (unsigned)-1;
        max98088->eq_textcnt = 0;
 
        cdata = &max98088->dai[0];
