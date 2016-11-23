8dca338beac57160f7eb2706199565624257a514
Pedro Perez de Heredia <pedro.perez@digi.com>
2016-11-23 13:43:43 +0100

    regulators: pfuze100: change mode for SW1A and SW2 to APS/PFM
    Configure SW1A and SW2 buck regulators as APS/PFM, otherwise
    the wireless interface could fail during suspend/resume.
    
    https://jira.digi.com/browse/DEL-3151
    
    Signed-off-by: Pedro Perez de Heredia <pedro.perez@digi.com>

--- drivers/regulator/pfuze100-regulator.c
+++ drivers/regulator/pfuze100-regulator.c
@@ -65,6 +65,10 @@
 #define PFUZE100_VGEN5VOL	0x70
 #define PFUZE100_VGEN6VOL	0x71
 
+#define PFUZE100_SW1AMODE	0x23
+#define PFUZE100_SW2MODE	0x38
+#define PFUZE100_BUCK_APS_PFM	0x0c
+
 enum chips { PFUZE100, PFUZE200, PFUZE3000 = 3 };
 
 struct pfuze_regulator {
@@ -605,6 +609,24 @@ static int pfuze100_regulator_probe(struct i2c_client *client,
 	dev_info(&client->dev, "pfuze%s found.\n",
 		(pfuze_chip->chip_id == PFUZE100) ? "100" :
 		((pfuze_chip->chip_id == PFUZE200) ? "200" : "3000"));
+
+	if (of_machine_is_compatible("digi,ccimx6ul")) {
+		/*
+		 * Configure SW1A and SW2 buck regulators as APS/PFM, otherwise
+		 * the wireless interface could fail during suspend/resume.
+		 */
+		ret = regmap_write(pfuze_chip->regmap, PFUZE100_SW1AMODE,
+				   PFUZE100_BUCK_APS_PFM);
+		if (ret)
+			dev_warn(&client->dev,
+				 "error writing SW1AMODE register (%d)\n", ret);
+
+		ret = regmap_write(pfuze_chip->regmap, PFUZE100_SW2MODE,
+				   PFUZE100_BUCK_APS_PFM);
+		if (ret)
+			dev_warn(&client->dev,
+				 "error writing SW2MODE register (%d)\n", ret);
+	}
 
 	memcpy(pfuze_chip->regulator_descs, pfuze_regulators,
 		sizeof(pfuze_chip->regulator_descs));
