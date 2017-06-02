2a421217d2de2430135b311dd0a3fa0cee6454ec
Gottfried Haider <gottfried.haider@gmail.com>
2017-06-02 10:05:21 +0000

    pwm: Set class for exported channels in sysfs
    Notifications for devices without bus or class set get dropped by
    dev_uevent_filter. Adding the class to the exported child matches
    what the gpio subsystem is doing.
    
    With this change exporting a channel triggers a udev event, which
    gives userspace a chance to fixup permissions and makes it possible
    for non-root users to make use of the pwm subsystem.
    
    Signed-off-by: Gottfried Haider <gottfried.haider@gmail.com>
    CC: Thierry Reding <thierry.reding@gmail.com>
    CC: H Hartley Sweeten <hsweeten@visionengravers.com>
    CC: linux-pwm@vger.kernel.org
    CC: linux-arm-kernel@lists.infradead.org
    CC: linux-rpi-kernel@lists.infradead.org
    Signed-off-by: Mike Engel <Mike.Engel@digi.com>

--- drivers/pwm/sysfs.c
+++ drivers/pwm/sysfs.c
@@ -194,6 +194,7 @@ static int pwm_export_child(struct device *parent, struct pwm_device *pwm)
 
 	export->pwm = pwm;
 
+	export->child.class = parent->class;
 	export->child.release = pwm_export_release;
 	export->child.parent = parent;
 	export->child.devt = MKDEV(0, 0);
