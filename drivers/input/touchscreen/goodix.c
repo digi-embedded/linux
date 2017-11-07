e735a7e9b8d021d9f5fc0550bf4d6c22de2c9bc8
Pedro Perez de Heredia <pedro.perez@digi.com>
2017-11-07 12:16:02 +0100

    drivers: goodix: decouple irq and reset gpios
    The Goodix touch controller allows the use of two optional gpios (RESET and
    INT) to reset the touch controller, select the i2c address of the device and
    exit the device from sleep mode.
    
    This commit allows the use of the INT gpio without the RESET gpio. The pre-
    vious implementation required that both were provided simultaneously.
    
    Signed-off-by: Pedro Perez de Heredia <pedro.perez@digi.com>
    (cherry picked from commit 9e111026d9be320a84a350a85f790ce92a594d86)

--- drivers/input/touchscreen/goodix.c
+++ drivers/input/touchscreen/goodix.c
@@ -1204,7 +1204,7 @@ reset:
 
 	ts->chip = goodix_get_chip_data(ts->id);
 
-	if (ts->load_cfg_from_disk &&
+	if (ts->gpiod_int && 
 	    !device_property_read_bool(&client->dev, "skip-firmware-request")) {
 		/* update device config */
 		ts->cfg_name = devm_kasprintf(&client->dev, GFP_KERNEL,
@@ -1249,7 +1249,7 @@ static int goodix_ts_remove(struct i2c_client *client)
 	struct goodix_ts_data *ts = i2c_get_clientdata(client);
 	int error;
 
-	if (ts->load_cfg_from_disk)
+	if (ts->gpiod_int)
 		wait_for_completion(&ts->firmware_loading_complete);
 
 	kfree(ts->cfg.data);
@@ -1275,7 +1275,7 @@ static int __maybe_unused goodix_suspend(struct device *dev)
 		wait_for_completion(&ts->firmware_loading_complete);
 
 	/* We need gpio pins to suspend/resume */
-	if (ts->irq_pin_access_method == IRQ_PIN_ACCESS_NONE) {
+	if (!ts->gpiod_int) {
 		disable_irq(client->irq);
 		return 0;
 	}
