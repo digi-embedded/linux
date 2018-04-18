c2a4d81709de6ac66fce19e441d3b152dc35cca1
Amelie Delaunay <amelie.delaunay@foss.st.com>
2018-04-18 16:46:09 +0200

    dt-bindings: rtc: stm32: add st,lsco optional property to select output
    STM32 RTC has three output pins: RTC_OUT1, RTC_OUT2 or RTC_OUT2_RMP.
    
    RTC Low-Speed Clock Output (LSCO) can be output on RTC_OUT1 or
    RTC_OUT2_RMP.
    
    This patch adds constants for RTC output bindings and adds st,lsco
    optional property for stm32 rtc driver, to select and enable LSCO.
    A pinctrl state is also optional to reserve pin for RTC output.
    
    Signed-off-by: Amelie Delaunay <amelie.delaunay@foss.st.com>
    Signed-off-by: Valentin Caron <valentin.caron@foss.st.com>
    Change-Id: Ie6fa201f22dd726f9d91e1a9a195a98e5e6213b0

---
+++ include/dt-bindings/rtc/rtc-stm32.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * This header provides constants for STM32_RTC bindings.
+ */
+
+#ifndef _DT_BINDINGS_RTC_RTC_STM32_H
+#define _DT_BINDINGS_RTC_RTC_STM32_H
+
+#define RTC_NO_OUT	0
+#define RTC_OUT1	1
+#define RTC_OUT2	2
+#define RTC_OUT2_RMP	3
+
+#endif
