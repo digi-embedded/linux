8930eafed4fd73445c5fd3233eabe63c55f77f63
Hector Palacios <hector.palacios@digi.com>
2018-05-08 10:23:01 +0200

    serial: imx: wrap use of digi_get_board_version()
    This function is not available for i.MX8QXP at the moment
    and the code is where it is used is not applicable anyway.
    
    Signed-off-by: Hector Palacios <hector.palacios@digi.com>

--- drivers/tty/serial/imx.c
+++ drivers/tty/serial/imx.c
@@ -192,7 +192,11 @@
 #define IMX_RXBD_NUM 20
 #define IMX_MODULE_MAX_CLK_RATE	80000000
 
+#if defined(CONFIG_SOC_IMX6Q) || defined(CONFIG_SOC_IMX6UL)
 extern int digi_get_board_version(void);
+#else
+int digi_get_board_version(void) { return 0; }
+#endif
 
 /* i.MX21 type uart runs on all i.mx except i.MX1 and i.MX6q */
 enum imx_uart_type {
