19b02aeaa2074e41a185e197df51cd3d4276a5fc
Hector Bujanda <hector.bujanda@digi.com>
2018-10-09 16:44:51 +0200

    serial: fsl_uart: fix set_mctrl() and get_mctrl() functions
    According to documentation:
       - set_mctrl() function should only handle TIOCM_RTS and TIOCM_LOOP flags;
       - get_mctrl() function should only handle TIOCM_CTS flag;
    
    Previous implementation was incorrectly handling all flags in both functions.
    
    https://jira.digi.com/browse/DEL-6134
    
    Signed-off-by: Hector Bujanda <hector.bujanda@digi.com>
    (cherry picked from commit 9bd39cc81194a33bebea9d31c272260adf3b6f9d)
    (cherry picked from commit 7b94646c88483e2ab927df08f91901426e5d5e51)

--- drivers/tty/serial/fsl_lpuart.c
+++ drivers/tty/serial/fsl_lpuart.c
@@ -1540,9 +1540,6 @@ static unsigned int lpuart32_get_mctrl(struct uart_port *port)
 	if (reg & UARTMODIR_TXCTSE)
 		temp |= TIOCM_CTS;
 
-	if (reg & UARTMODIR_RXRTSE)
-		temp |= TIOCM_RTS;
-
 	return temp;
 }
 
@@ -1593,13 +1590,10 @@ static void lpuart32_set_mctrl(struct uart_port *port, unsigned int mctrl)
 	/* Make sure RXRTSE bit is not set when RS485 is enabled */
 	if (!(port->rs485.flags & SER_RS485_ENABLED)) {
 		temp = lpuart32_read(port, UARTMODIR) &
-			~(UARTMODIR_RXRTSE | UARTMODIR_TXCTSE);
+			~UARTMODIR_RXRTSE;
 
 		if (mctrl & TIOCM_RTS)
 			temp |= UARTMODIR_RXRTSE;
-
-		if (mctrl & TIOCM_CTS)
-			temp |= UARTMODIR_TXCTSE;
 
 		lpuart32_write(port, temp, UARTMODIR);
 	}
