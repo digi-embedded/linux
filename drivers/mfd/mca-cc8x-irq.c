9f0e9db06931f067ce0df193385ed028dab0e412
Mike Engel <Mike.Engel@digi.com>
2019-01-07 18:09:44 +0100

    mfd: mca_cc8x: add MCA core
    Add MCA core support for ConnectCore 8X SoM.
    
    Signed-off-by: Mike Engel <Mike.Engel@digi.com>

---
+++ drivers/mfd/mca-cc8x-irq.c
@@ -0,0 +1,143 @@
+/*
+ *  Copyright 2018 Digi International Inc
+ *
+ *  This program is free software; you can redistribute  it and/or modify it
+ *  under  the terms of  the GNU General  Public License as published by the
+ *  Free Software Foundation;  either version 2 of the  License, or (at your
+ *  option) any later version.
+ */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/irq.h>
+#include <linux/mfd/core.h>
+#include <linux/interrupt.h>
+#include <linux/regmap.h>
+#include <linux/mfd/mca-common/core.h>
+#include <linux/mfd/mca-cc8x/core.h>
+
+#include <soc/imx8/sc/svc/irq/api.h>
+
+#define MCA_CC8X_IRQ_0_OFFSET		0
+#define MCA_CC8X_IRQ_1_OFFSET		1
+#define MCA_CC8X_IRQ_2_OFFSET		2
+#define MCA_CC8X_IRQ_3_OFFSET		3
+
+static const struct regmap_irq mca_cc8x_irqs[] = {
+	/* MCA irqs A register */
+	[MCA_CC8X_IRQ_RTC_ALARM] = {
+                .reg_offset = MCA_CC8X_IRQ_0_OFFSET,
+                .mask = MCA_CC8X_M_RTC_ALARM,
+        },
+	[MCA_CC8X_IRQ_RTC_1HZ] = {
+                .reg_offset = MCA_CC8X_IRQ_0_OFFSET,
+                .mask = MCA_CC8X_M_RTC_1HZ,
+        },
+	[MCA_CC8X_IRQ_WATCHDOG] = {
+                .reg_offset = MCA_CC8X_IRQ_0_OFFSET,
+                .mask = MCA_CC8X_M_WATCHDOG,
+        },
+	[MCA_CC8X_IRQ_PWR_SLEEP] = {
+                .reg_offset = MCA_CC8X_IRQ_0_OFFSET,
+                .mask = MCA_CC8X_M_PWR_SLEEP,
+        },
+	[MCA_CC8X_IRQ_PWR_OFF] = {
+                .reg_offset = MCA_CC8X_IRQ_0_OFFSET,
+                .mask = MCA_CC8X_M_PWR_OFF,
+        },
+	[MCA_CC8X_IRQ_TAMPER0] = {
+		.reg_offset = MCA_CC8X_IRQ_0_OFFSET,
+		.mask = MCA_CC8X_M_TAMPER0,
+	},
+	[MCA_CC8X_IRQ_TAMPER1] = {
+		.reg_offset = MCA_CC8X_IRQ_0_OFFSET,
+		.mask = MCA_CC8X_M_TAMPER1,
+	},
+	[MCA_CC8X_IRQ_ADC] = {
+		.reg_offset = MCA_CC8X_IRQ_0_OFFSET,
+		.mask = MCA_CC8X_M_ADC,
+	},
+	[MCA_CC8X_IRQ_GPIO_BANK_0] = {
+		.reg_offset = MCA_CC8X_IRQ_1_OFFSET,
+		.mask = MCA_GPIO_BANK_0,
+	},
+	[MCA_CC8X_IRQ_GPIO_BANK_1] = {
+		.reg_offset = MCA_CC8X_IRQ_1_OFFSET,
+		.mask = MCA_GPIO_BANK_1,
+	},
+	[MCA_CC8X_IRQ_GPIO_BANK_2] = {
+		.reg_offset = MCA_CC8X_IRQ_1_OFFSET,
+		.mask = MCA_GPIO_BANK_2,
+	},
+	[MCA_CC8X_IRQ_TAMPER2] = {
+		.reg_offset = MCA_CC8X_IRQ_2_OFFSET,
+		.mask = MCA_CC8X_M_TAMPER2,
+	},
+	[MCA_CC8X_IRQ_TAMPER3] = {
+		.reg_offset = MCA_CC8X_IRQ_2_OFFSET,
+		.mask = MCA_CC8X_M_TAMPER3,
+	},
+	[MCA_CC8X_IRQ_UART] = {
+		.reg_offset = MCA_CC8X_IRQ_2_OFFSET,
+		.mask = MCA_CC8X_M_UART,
+	},
+};
+
+/*
+ * This hook is used to call the SCU MU interrupt handler to manage the SC
+ * interrupts that could have been signaled by the SCU.
+ * This is needed because the MCA ISR replaces the original SCU MU ISR because
+ * both can not be registered as shared irq handlers.
+ */
+static int mca_cc8x_pre_irq(void *irq_drv_data)
+{
+	return imx8_scu_mu_isr(0, NULL);
+}
+
+static const struct regmap_irq_chip mca_cc8x_irq_chip = {
+	.name		= "mca-cc8x-irq",
+	.irqs		= mca_cc8x_irqs,
+	.num_irqs	= ARRAY_SIZE(mca_cc8x_irqs),
+	.num_regs	= MCA_NUM_IRQ_REGS,
+	.status_base	= MCA_IRQ_STATUS_0,
+	.mask_base	= MCA_CC8X_IRQ_MASK_0,
+	.ack_base	= MCA_IRQ_STATUS_0,
+	.handle_pre_irq	= mca_cc8x_pre_irq,
+	.init_ack_masked = true,
+};
+
+int mca_cc8x_irq_init(struct mca_drv *mca)
+{
+	int ret;
+
+	if (!mca->chip_irq) {
+		dev_err(mca->dev, "No IRQ configured\n");
+		return -EINVAL;
+	}
+
+	mca->irq_base = -1;
+	ret = regmap_add_irq_chip(mca->regmap, mca->chip_irq,
+				  IRQF_TRIGGER_LOW | IRQF_ONESHOT | IRQF_SHARED,
+				  mca->irq_base, &mca_cc8x_irq_chip,
+				  &mca->regmap_irq);
+	if (ret) {
+		dev_err(mca->dev, "Failed to reguest IRQ %d: %d\n",
+			mca->chip_irq, ret);
+		return ret;
+	}
+
+	ret = imx8_mu_enable_sc_irqs(SC_IRQ_TEMP_MCA | SC_IRQ_TEMP_PMIC0_HIGH | \
+				     SC_IRQ_TEMP_PMIC1_HIGH,
+				     SC_IRQ_RTC,
+				     SC_IRQ_BUTTON,
+				     SC_IRQ_WDOG);
+	if (ret)
+		dev_err(mca->dev, "Failed to reguest SC MU IRQs (%d)\n", ret);
+
+	return ret;
+}
+
+void mca_cc8x_irq_exit(struct mca_drv *mca)
+{
+        regmap_del_irq_chip(mca->chip_irq, mca->regmap_irq);
+}
---
+++ include/linux/mfd/mca-common/core.h
@@ -0,0 +1,89 @@
+/*
+ *  Copyright 2017-2018 Digi International Inc
+ *
+ *  This program is free software; you can redistribute  it and/or modify it
+ *  under  the terms of  the GNU General  Public License as published by the
+ *  Free Software Foundation;  either version 2 of the  License, or (at your
+ *  option) any later version.
+ */
+
+#ifndef MFD_MCA_COMMON_CORE_H_
+#define MFD_MCA_COMMON_CORE_H_
+
+#include <linux/interrupt.h>
+#include <linux/platform_device.h>
+#include <linux/mfd/mca-common/registers.h>
+#include <linux/gpio.h>
+
+#define MCA_MAKE_FW_VER(a,b)		(u16)(((a) << 8) | ((b) & 0xff))
+#define MCA_FW_VER_MAJOR(v)		(((v) >> 8) & 0xff)
+#define MCA_FW_VER_MINOR(v)		((v) & 0xff)
+#define MCA_FW_VER_ALPHA_MASK		BIT(15)
+
+#define MCA_IRQ_GPIO_BANK_0_NAME	"GPIO_BANK0"
+#define MCA_IRQ_GPIO_BANK_1_NAME	"GPIO_BANK1"
+#define MCA_IRQ_GPIO_BANK_2_NAME	"GPIO_BANK2"
+#define MCA_IRQ_GPIO_BANK_3_NAME	"GPIO_BANK3"
+#define MCA_IRQ_GPIO_BANK_4_NAME	"GPIO_BANK4"
+#define MCA_IRQ_GPIO_BANK_5_NAME	"GPIO_BANK5"
+#define MCA_IRQ_RTC_ALARM_NAME		"RTC ALARM"
+#define MCA_IRQ_RTC_1HZ_NAME		"RTC 1HZ"
+#define MCA_IRQ_PWR_SLEEP_NAME		"SLEEP"
+#define MCA_IRQ_PWR_OFF_NAME		"PWR OFF"
+#define MCA_IRQ_WATCHDOG_NAME		"WATCHDOG"
+
+/* Number of interrupt registers */
+#define MCA_NUM_IRQ_REGS		4
+/* Max number of IOs */
+#define MCA_MAX_IOS			64
+
+#define MCA_MAX_GPIO_IRQ_BANKS		6
+
+#define MCA_MAX_IO_BYTES		((MCA_MAX_IOS + 7) / 8)
+
+struct mca_drv {
+	struct device *dev;
+	u8 dev_id;
+	u8 hw_version;
+	bool fw_is_alpha;
+	u16 fw_version;
+	u32 flags;
+	struct regmap *regmap;
+	struct regmap_irq_chip_data *regmap_irq;
+	struct notifier_block restart_handler;
+	int chip_irq;
+	u32 irq_base;
+	int gpio_base;
+	int fw_update_gpio;
+	int som_hv;
+	u32 last_mca_reset;
+	u32 last_mpu_reset;
+	struct bin_attribute *nvram;
+	struct device *i2c_adapter_dev;
+};
+
+struct mca_adc {
+	struct device *dev;
+	struct regmap *regmap;
+	u32 vref;
+	int irq;
+};
+
+struct mca_gpio {
+	void * parent;
+	struct regmap *regmap;
+	struct device *dev;
+	struct gpio_chip gc;
+	struct mutex irq_lock;
+	uint8_t irq_cfg[MCA_MAX_IOS];
+	uint8_t irq_capable[MCA_MAX_IO_BYTES];
+	int irq[MCA_MAX_GPIO_IRQ_BANKS];
+};
+
+int mca_adc_probe(struct platform_device *pdev, struct device *mca_dev,
+		  struct regmap *regmap, int gpio_base, char const *dt_compat);
+int mca_adc_remove(struct platform_device *pdev, int gpio_base);
+int mca_gpio_probe(struct platform_device *pdev, struct device *mca_dev,
+		   struct regmap *regmap, int *gpio_base, char const *dt_compat);
+
+#endif /* MFD_MCA_COMMON_CORE_H_ */
