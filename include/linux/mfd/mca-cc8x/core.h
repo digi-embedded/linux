9557e8fb22d93f0fe07332eb4fef515d8f3f54d4
Mike Engel <Mike.Engel@digi.com>
2019-01-07 18:09:44 +0100

    mfd: mca_cc8x: add MCA core
    Add MCA core support for ConnectCore 8X SoM.
    
    Signed-off-by: Mike Engel <Mike.Engel@digi.com>

---
+++ include/linux/mfd/mca-cc8x/core.h
@@ -0,0 +1,68 @@
+/*
+ *  Copyright 2018 Digi International Inc
+ *
+ *  This program is free software; you can redistribute  it and/or modify it
+ *  under  the terms of  the GNU General  Public License as published by the
+ *  Free Software Foundation;  either version 2 of the  License, or (at your
+ *  option) any later version.
+ */
+
+#ifndef MFD_MCA_CC8X_CORE_H_
+#define MFD_MCA_CC8X_CORE_H_
+
+#include <linux/interrupt.h>
+#include <linux/mfd/mca-common/core.h>
+#include <linux/mfd/mca-cc8x/registers.h>
+
+/* MCA CC8X modules */
+#define MCA_CC8X_DRVNAME_CORE		"mca-cc8x-core"
+#define MCA_CC8X_DRVNAME_RTC		"mca-cc8x-rtc"
+#define MCA_CC8X_DRVNAME_WATCHDOG	"mca-cc8x-watchdog"
+#define MCA_CC8X_DRVNAME_GPIO		"mca-cc8x-gpio"
+#define MCA_CC8X_DRVNAME_PWRKEY		"mca-cc8x-pwrkey"
+#define MCA_CC8X_DRVNAME_ADC		"mca-cc8x-adc"
+#define MCA_CC8X_DRVNAME_TAMPER		"mca-cc8x-tamper"
+#define MCA_CC8X_DRVNAME_COMPARATOR	"mca-cc8x-comparator"
+#define MCA_CC8X_DRVNAME_UART		"mca-cc8x-uart"
+#define MCA_CC8X_DRVNAME_PWM		"mca-pwm"
+
+#define MCA_CC8X_DEVICE_ID_VAL		0x4A
+
+/* Interrupts */
+enum mca_cc8x_irqs {
+	MCA_CC8X_IRQ_RTC_ALARM,
+	MCA_CC8X_IRQ_RTC_1HZ,
+	MCA_CC8X_IRQ_WATCHDOG,
+	MCA_CC8X_IRQ_PWR_SLEEP,
+	MCA_CC8X_IRQ_PWR_OFF,
+	MCA_CC8X_IRQ_TAMPER0,
+	MCA_CC8X_IRQ_TAMPER1,
+	MCA_CC8X_IRQ_ADC,
+	MCA_CC8X_IRQ_GPIO_BANK_0,
+	MCA_CC8X_IRQ_GPIO_BANK_1,
+	MCA_CC8X_IRQ_GPIO_BANK_2,
+	MCA_CC8X_IRQ_TAMPER2,
+	MCA_CC8X_IRQ_TAMPER3,
+	MCA_CC8X_IRQ_UART,
+	/* ... */
+
+	MCA_CC8X_NUM_IRQS,
+};
+
+#define MCA_CC8X_IRQ_TAMPER0_NAME		"TAMPER0"
+#define MCA_CC8X_IRQ_TAMPER1_NAME		"TAMPER1"
+#define MCA_CC8X_IRQ_TAMPER2_NAME		"TAMPER2"
+#define MCA_CC8X_IRQ_TAMPER3_NAME		"TAMPER3"
+#define MCA_CC8X_IRQ_ADC_NAME			"ADC"
+#define MCA_CC8X_IRQ_UART_NAME			"UART"
+
+
+
+int mca_cc8x_device_init(struct mca_drv *mca, u32 irq);
+int mca_cc8x_irq_init(struct mca_drv *mca);
+void mca_cc8x_device_exit(struct mca_drv *mca);
+void mca_cc8x_irq_exit(struct mca_drv *mca);
+int mca_cc8x_suspend(struct device *dev);
+int mca_cc8x_resume(struct device *dev);
+
+#endif /* MFD_MCA_CC8X_CORE_H_ */
