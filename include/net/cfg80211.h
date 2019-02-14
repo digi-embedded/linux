de3c4837f529fa366e92b1e271cec546890d933f
Chung-Hsien Hsu <stanley.hsu@cypress.com>
2019-02-14 16:33:53 +0800

    nl80211: add authorized flag back to ROAM event
    Commit 503c1fb98ba3 ("cfg80211/nl80211: add a port authorized event")
    added the NL80211_CMD_PORT_AUTHORIZED event to indicate that a
    connection is authorized. It replaced the PORT_AUTHORIZED attribute and
    the authorized flag added in commit f45cbe6e691f
    ("nl80211: add authorized flag to ROAM event").
    
    However, for offload FT, using PORT_AUTHORIZED event mechanism induces
    wpa_supplicant to start a full EAP exchange after a successful roaming.
    This patch adds the flag mechanism back to the ROAM event for drivers
    to fix the offload FT roaming issue.
    
    Signed-off-by: Chung-Hsien Hsu <stanley.hsu@cypress.com>
    Signed-off-by: Chi-hsien Lin <chi-hsien.lin@cypress.com>

--- include/net/cfg80211.h
+++ include/net/cfg80211.h
@@ -7743,6 +7743,9 @@ cfg80211_connect_timeout(struct net_device *dev, const u8 *bssid,
  * @links.bss: For MLO roaming, entry of new bss to which STA link got
  *	roamed. For non-MLO roaming, links[0].bss points to entry of bss to
  *	which STA got roamed (may be %NULL if %links.bssid is set)
+ * @authorized: true if the 802.1X authentication was done by the driver or is
+ *	not needed (e.g., when Fast Transition protocol was used), false
+ *	otherwise. Ignored for networks that don't use 802.1X authentication.
  */
 struct cfg80211_roam_info {
 	const u8 *req_ie;
@@ -7750,7 +7753,6 @@ struct cfg80211_roam_info {
 	const u8 *resp_ie;
 	size_t resp_ie_len;
 	struct cfg80211_fils_resp_params fils;
-
 	const u8 *ap_mld_addr;
 	u16 valid_links;
 	struct {
@@ -7759,6 +7761,7 @@ struct cfg80211_roam_info {
 		struct ieee80211_channel *channel;
 		struct cfg80211_bss *bss;
 	} links[IEEE80211_MLD_MAX_NUM_LINKS];
+	bool authorized;
 };
 
 /**
