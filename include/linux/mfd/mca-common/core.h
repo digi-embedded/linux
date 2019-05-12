c654c2a90448c80a87baf2b3e47c215404f4b9e9
Pedro Perez de Heredia <pedro.perez@digi.com>
2019-05-12 22:05:59 +0200

    drivers: mfd: mca: implement reboot with a syscore callback
    The previous mca reboot implementation using a restart_handler was causing
    problems because the callback is executed in atomic context. The new one uses
    the shutdown syscore callback instead.
    
    https://jira.digi.com/browse/DEL-6501
    
    Signed-off-by: Pedro Perez de Heredia <pedro.perez@digi.com>
    (cherry picked from commit 0bfac900df5c81bc91faea6aa3bfa1bea27222c3)
    (cherry picked from commit 73db23e18e713a04129e8825a30140cc66a4615d)

--- include/linux/mfd/mca-common/core.h
+++ include/linux/mfd/mca-common/core.h
@@ -12,6 +12,7 @@
 
 #include <linux/interrupt.h>
 #include <linux/platform_device.h>
+#include <linux/syscore_ops.h>
 #include <linux/mfd/mca-common/registers.h>
 #include <linux/gpio.h>
 
@@ -59,7 +60,6 @@ struct mca_drv {
 	u32 flags;
 	struct regmap *regmap;
 	struct regmap_irq_chip_data *regmap_irq;
-	struct notifier_block restart_handler;
 	int chip_irq;
 	u32 irq_base;
 	int gpio_base;
@@ -69,6 +69,7 @@ struct mca_drv {
 	u32 last_mpu_reset;
 	struct bin_attribute *nvram;
 	struct device *i2c_adapter_dev;
+	struct syscore_ops syscore;
 };
 
 #endif /* MFD_MCA_COMMON_CORE_H_ */
