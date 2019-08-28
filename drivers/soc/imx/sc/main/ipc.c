f0e881a9dd28b70bdfb0f6b7b5709c2f28e1a64d
Hector Bujanda <hector.bujanda@digi.com>
2019-08-28 09:31:59 +0200

    Revert "MLK-21078-3 soc: imx: enable RX interrupt for IPC response"
    This reverts commit 20c60c1659297df6f885077cb8b16dd4003f0ec5.
    
    This commit together with 9f9da638686b03beb4cb632eeb6f74345d7c96c1 were making
    CC8X module hang very early during kernel initialization.
    Digi has changed the behavior of IPC since the MCA interrupts hang also from IPC.
    The changes introduced by these two commits seem to require the interrupt handling
    ready very early while the DIGI customization delays it a couple of seconds.
    
    This revert is temporal and MUST be revisited to make assure IPC communication!
    
    https://jira.digi.com/browse/DEL-6721
    
    Signed-off-by: Hector Bujanda <hector.bujanda@digi.com>
    (cherry picked from commit 5202bc8211a61dc755341a28dceeb06a9f8d5cd2)

--- drivers/soc/imx/sc/main/ipc.c
+++ drivers/soc/imx/sc/main/ipc.c
@@ -7,7 +7,6 @@
 
 /* Includes */
 #include <linux/arm-smccc.h>
-#include <linux/completion.h>
 #include <linux/err.h>
 #include <linux/kernel.h>
 #include <linux/of.h>
@@ -19,7 +18,6 @@
 #include <linux/irq.h>
 #include <linux/mx8_mu.h>
 #include <linux/syscore_ops.h>
-#include <linux/suspend.h>
 
 #include <soc/imx/fsl_hvc.h>
 #include <soc/imx8/sc/svc/irq/api.h>
@@ -41,9 +39,7 @@ static sc_ipc_t mu_ipcHandle;
 
 /* Local variables */
 static uint32_t gIPCport;
-static sc_rpc_msg_t *rx_msg;
 static bool scu_mu_init;
-struct completion rx_completion;
 
 DEFINE_MUTEX(scu_mu_mutex);
 
@@ -61,7 +57,6 @@ EXPORT_SYMBOL(sc_pm_set_clock_rate);
 void sc_call_rpc(sc_ipc_t handle, sc_rpc_msg_t *msg, sc_bool_t no_resp)
 {
 	struct arm_smccc_res res;
-	unsigned long timeout;
 
 	if (in_interrupt()) {
 		pr_warn("Cannot make SC IPC calls from an interrupt context\n");
@@ -70,8 +65,6 @@ void sc_call_rpc(sc_ipc_t handle, sc_rpc_msg_t *msg, sc_bool_t no_resp)
 	}
 	mutex_lock(&scu_mu_mutex);
 
-	reinit_completion(&rx_completion);
-	rx_msg = msg;
 	if (xen_initial_domain()) {
 		arm_smccc_hvc(FSL_HVC_SC, (uint64_t)msg, no_resp, 0, 0, 0, 0,
 			      0, &res);
@@ -79,14 +72,8 @@ void sc_call_rpc(sc_ipc_t handle, sc_rpc_msg_t *msg, sc_bool_t no_resp)
 			printk("Error FSL_HVC_SC %ld\n", res.a0);
 	} else {
 		sc_ipc_write(handle, msg);
-		if (!no_resp) {
-			timeout = wait_for_completion_timeout(&rx_completion, HZ / 10);
-			if (!timeout) {
-				pr_err("Timeout for IPC response!\n");
-				mutex_unlock(&scu_mu_mutex);
-				return;
-			}
-		}
+		if (!no_resp)
+			sc_ipc_read(handle, msg);
 	}
 
 	mutex_unlock(&scu_mu_mutex);
@@ -298,12 +285,6 @@ irqreturn_t imx8_scu_mu_isr(int irq, void *param)
 {
 	u32 irqs;
 
-	irqs = (readl_relaxed(mu_base_virtaddr + 0x20) & (0xf << 24));
-	if (irqs) {
-		sc_ipc_read(mu_ipcHandle, rx_msg);
-		complete(&rx_completion);
-	}
-
 	irqs = (readl_relaxed(mu_base_virtaddr + 0x20) & (0xf << 28));
 	if (irqs) {
 		/* Clear the General Interrupt */
@@ -378,7 +359,6 @@ static void imx8_mu_resume(void)
 	int i;
 
 	MU_Init(mu_base_virtaddr);
-	MU_EnableRxFullInt(mu_base_virtaddr, 0);
 	for (i = 0; i < MU_RR_COUNT; i++)
 		MU_EnableGeneralInt(mu_base_virtaddr, i);
 }
@@ -441,7 +421,6 @@ int __init imx8_mu_init(void)
 
 		/* Init MU */
 		MU_Init(mu_base_virtaddr);
-		MU_EnableRxFullInt(mu_base_virtaddr, 0);
 
 #if 1
 		/* Enable all RX interrupts */
@@ -450,7 +429,6 @@ int __init imx8_mu_init(void)
 #endif
 		gIPCport = scu_mu_id;
 		scu_mu_init = true;
-		init_completion(&rx_completion);
 	}
 
 	sciErr = sc_ipc_open(&mu_ipcHandle, scu_mu_id);
