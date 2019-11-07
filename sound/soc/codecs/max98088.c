d54e7cfde5e42b02ce590345773bf05f50da5757
Mike Engel <Mike.Engel@digi.com>
2019-11-07 15:21:56 +0100

    sound: max98088: Add 1V8 regulator support to audio codec
    This commit adds support for the 1V8 regulator from our SOM
    connected to the audio codec.
    
    Signed-off-by: Mike Engel <Mike.Engel@digi.com>

--- sound/soc/codecs/max98088.c
+++ sound/soc/codecs/max98088.c
@@ -42,6 +42,7 @@ struct max98088_cdata {
 struct max98088_priv {
 	struct regmap *regmap;
 	struct regulator *vcc;
+	struct regulator *dvdd;
 	enum max98088_type devtype;
 	struct max98088_pdata *pdata;
 	unsigned int sysclk;
@@ -1744,10 +1745,18 @@ static int max98088_i2c_suspend(struct device *dev)
 			dev_err(dev, "Failed to disable vcc (%d)\n", ret);
 			return ret;
 		}
-
-		regcache_mark_dirty(max98088->regmap);
-		regcache_cache_only(max98088->regmap, true);
 	}
+
+	if (max98088->dvdd) {
+		ret = regulator_disable(max98088->dvdd);
+		if (ret) {
+			dev_err(dev, "Failed to disable dvdd (%d)\n", ret);
+			return ret;
+		}
+	}
+
+	regcache_mark_dirty(max98088->regmap);
+	regcache_cache_only(max98088->regmap, true);
 
 	return 0;
 }
@@ -1756,6 +1765,14 @@ static int max98088_i2c_resume(struct device *dev)
 {
 	struct max98088_priv *max98088 = dev_get_drvdata(dev);
 	int ret;
+
+	if (max98088->dvdd) {
+		ret = regulator_enable(max98088->dvdd);
+		if (ret) {
+			dev_err(dev, "Failed to enable dvdd (%d)\n", ret);
+			return ret;
+		}
+	}
 
 	if (max98088->vcc) {
 		ret = regulator_enable(max98088->vcc);
@@ -1809,6 +1826,24 @@ static int max98088_i2c_probe(struct i2c_client *i2c,
 		}
 	}
 
+	max98088->dvdd = devm_regulator_get_optional(&i2c->dev, "dvdd");
+	if (IS_ERR(max98088->dvdd)) {
+		if (PTR_ERR(max98088->dvdd) == -EPROBE_DEFER) {
+			return -EPROBE_DEFER;
+		}
+
+		max98088->dvdd = NULL;
+		dev_dbg(&i2c->dev, "No dvdd codec regulator\n");
+	}
+
+	if (max98088->dvdd) {
+		ret = regulator_enable(max98088->dvdd);
+		if (ret) {
+			dev_err(&i2c->dev, "Failed to enable dvdd regulator\n");
+			goto free_mem;
+		}
+	}
+
 	max98088->regmap = devm_regmap_init_i2c(i2c, &max98088_regmap);
        if (IS_ERR(max98088->regmap))
 	       return PTR_ERR(max98088->regmap);
@@ -1837,6 +1872,8 @@ static int max98088_i2c_remove(struct i2c_client *client)
 
 	if (max98088->vcc)
 		regulator_disable(max98088->vcc);
+	if (max98088->dvdd)
+		regulator_disable(max98088->dvdd);
 
        return 0;
 }
