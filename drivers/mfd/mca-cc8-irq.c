211c4323a92e7f78ebdc88e2ab5d2f501a8ad6bd
Hector Bujanda <hector.bujanda@digi.com>
2020-01-09 18:00:50 +0100

    ARM64: mca-cc8: generalize CC8X and CC8M platforms
    Renamed 'mca-cc8x' to 'mca-cc8' in:
       - Kconfig files
       - Makefile files
       - File names (*.c and *.h)
       - Source code.
    
    Added specific "digi,mca_cc8m" device tree entries.
    Added specific "mca-cc8m-xxx" driver entries.
    
    https://jira.digi.com/browse/DEL-6931
    
    Signed-off-by: Hector Bujanda <hector.bujanda@digi.com>
    (cherry picked from commit 5d78d02f4ad3ee5e6180803e1243feb35e333054)

--- drivers/mfd/mca-cc8-irq.c
+++ drivers/mfd/mca-cc8-irq.c
@@ -14,7 +14,7 @@
 #include <linux/interrupt.h>
 #include <linux/regmap.h>
 #include <linux/mfd/mca-common/core.h>
-#include <linux/mfd/mca-cc8x/core.h>
+#include <linux/mfd/mca-cc8/core.h>
 #include <soc/imx8/soc.h>
 
 #include <soc/imx8/sc/svc/irq/api.h>
@@ -24,73 +24,73 @@
 #define MCA_IRQ_2_OFFSET		2
 #define MCA_IRQ_3_OFFSET		3
 
-static const struct regmap_irq mca_cc8x_irqs[] = {
+static const struct regmap_irq mca_cc8_irqs[] = {
 	/* MCA irqs A register */
-	[MCA_CC8X_IRQ_RTC_ALARM] = {
+	[MCA_CC8_IRQ_RTC_ALARM] = {
                 .reg_offset = MCA_IRQ_0_OFFSET,
                 .mask = MCA_M_RTC_ALARM,
         },
-	[MCA_CC8X_IRQ_RTC_1HZ] = {
+	[MCA_CC8_IRQ_RTC_1HZ] = {
                 .reg_offset = MCA_IRQ_0_OFFSET,
                 .mask = MCA_M_RTC_1HZ,
         },
-	[MCA_CC8X_IRQ_WATCHDOG] = {
+	[MCA_CC8_IRQ_WATCHDOG] = {
                 .reg_offset = MCA_IRQ_0_OFFSET,
                 .mask = MCA_M_WATCHDOG,
         },
-	[MCA_CC8X_IRQ_PWR_SLEEP] = {
+	[MCA_CC8_IRQ_PWR_SLEEP] = {
                 .reg_offset = MCA_IRQ_0_OFFSET,
                 .mask = MCA_M_PWR_SLEEP,
         },
-	[MCA_CC8X_IRQ_PWR_OFF] = {
+	[MCA_CC8_IRQ_PWR_OFF] = {
                 .reg_offset = MCA_IRQ_0_OFFSET,
                 .mask = MCA_M_PWR_OFF,
         },
-	[MCA_CC8X_IRQ_TAMPER0] = {
+	[MCA_CC8_IRQ_TAMPER0] = {
 		.reg_offset = MCA_IRQ_0_OFFSET,
 		.mask = MCA_M_TAMPER0,
 	},
-	[MCA_CC8X_IRQ_TAMPER1] = {
+	[MCA_CC8_IRQ_TAMPER1] = {
 		.reg_offset = MCA_IRQ_0_OFFSET,
 		.mask = MCA_M_TAMPER1,
 	},
-	[MCA_CC8X_IRQ_ADC] = {
+	[MCA_CC8_IRQ_ADC] = {
 		.reg_offset = MCA_IRQ_0_OFFSET,
 		.mask = MCA_M_ADC,
 	},
-	[MCA_CC8X_IRQ_GPIO_BANK_0] = {
+	[MCA_CC8_IRQ_GPIO_BANK_0] = {
 		.reg_offset = MCA_IRQ_1_OFFSET,
 		.mask = MCA_GPIO_BANK_0,
 	},
-	[MCA_CC8X_IRQ_GPIO_BANK_1] = {
+	[MCA_CC8_IRQ_GPIO_BANK_1] = {
 		.reg_offset = MCA_IRQ_1_OFFSET,
 		.mask = MCA_GPIO_BANK_1,
 	},
-	[MCA_CC8X_IRQ_GPIO_BANK_2] = {
+	[MCA_CC8_IRQ_GPIO_BANK_2] = {
 		.reg_offset = MCA_IRQ_1_OFFSET,
 		.mask = MCA_GPIO_BANK_2,
 	},
-	[MCA_CC8X_IRQ_TAMPER2] = {
+	[MCA_CC8_IRQ_TAMPER2] = {
 		.reg_offset = MCA_IRQ_2_OFFSET,
 		.mask = MCA_M_TAMPER2,
 	},
-	[MCA_CC8X_IRQ_TAMPER3] = {
+	[MCA_CC8_IRQ_TAMPER3] = {
 		.reg_offset = MCA_IRQ_2_OFFSET,
 		.mask = MCA_M_TAMPER3,
 	},
-	[MCA_CC8X_IRQ_UART0] = {
+	[MCA_CC8_IRQ_UART0] = {
 		.reg_offset = MCA_IRQ_2_OFFSET,
 		.mask = MCA_M_UART0,
 	},
-	[MCA_CC8X_IRQ_UART1] = {
+	[MCA_CC8_IRQ_UART1] = {
 		.reg_offset = MCA_IRQ_2_OFFSET,
 		.mask = MCA_M_UART1,
 	},
-	[MCA_CC8X_IRQ_UART2] = {
+	[MCA_CC8_IRQ_UART2] = {
 		.reg_offset = MCA_IRQ_2_OFFSET,
 		.mask = MCA_M_UART2,
 	},
-	[MCA_CC8X_IRQ_RTC_PERIODIC_IRQ] = {
+	[MCA_CC8_IRQ_RTC_PERIODIC_IRQ] = {
 		.reg_offset = MCA_IRQ_2_OFFSET,
 		.mask = MCA_M_RTC_PERIODIC_IRQ,
 	},
@@ -107,10 +107,10 @@ static int mca_cc8x_pre_irq(void *irq_drv_data)
 	return imx8_scu_mu_isr(0, NULL);
 }
 
-static struct regmap_irq_chip mca_cc8x_irq_chip = {
-	.name		= "mca-cc8x-irq",
-	.irqs		= mca_cc8x_irqs,
-	.num_irqs	= ARRAY_SIZE(mca_cc8x_irqs),
+static struct regmap_irq_chip mca_cc8_irq_chip = {
+	.name		= "mca-cc8-irq",
+	.irqs		= mca_cc8_irqs,
+	.num_irqs	= ARRAY_SIZE(mca_cc8_irqs),
 	.num_regs	= MCA_NUM_IRQ_REGS,
 	.status_base	= MCA_IRQ_STATUS_0,
 	.mask_base	= MCA_IRQ_MASK_0,
@@ -118,7 +118,7 @@ static struct regmap_irq_chip mca_cc8x_irq_chip = {
 	.init_ack_masked = true,
 };
 
-int mca_cc8x_irq_init(struct mca_drv *mca)
+int mca_cc8_irq_init(struct mca_drv *mca)
 {
 	int ret, flags;
 
@@ -128,7 +128,7 @@ int mca_cc8x_irq_init(struct mca_drv *mca)
 	}
 
 	if (cpu_is_imx8qxp()) {
-		mca_cc8x_irq_chip.handle_pre_irq = mca_cc8x_pre_irq;
+		mca_cc8_irq_chip.handle_pre_irq = mca_cc8x_pre_irq;
 		flags = IRQF_ONESHOT | IRQF_SHARED;
 	} else {
 		flags = IRQF_TRIGGER_LOW | IRQF_ONESHOT | IRQF_SHARED;
@@ -136,7 +136,7 @@ int mca_cc8x_irq_init(struct mca_drv *mca)
 
 	mca->irq_base = -1;
 	ret = regmap_add_irq_chip(mca->regmap, mca->chip_irq, flags,
-				  mca->irq_base, &mca_cc8x_irq_chip,
+				  mca->irq_base, &mca_cc8_irq_chip,
 				  &mca->regmap_irq);
 	if (ret) {
 		dev_err(mca->dev, "Failed to reguest IRQ %d: %d\n",
@@ -157,7 +157,7 @@ int mca_cc8x_irq_init(struct mca_drv *mca)
 	return ret;
 }
 
-void mca_cc8x_irq_exit(struct mca_drv *mca)
+void mca_cc8_irq_exit(struct mca_drv *mca)
 {
         regmap_del_irq_chip(mca->chip_irq, mca->regmap_irq);
 }
