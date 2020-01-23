4f943e55629d26d6d4208c8dffc5a93063b888d4
Arnaud Pouliquen <arnaud.pouliquen@foss.st.com>
2020-01-23 11:28:19 +0100

    remoteproc: stm32: add shutdown ops
    User has to decide if the remote firmware should be stop or not on
    shutdown. By default the firmware is not stopped, only a warning is
    displayed for debug.
    
    Signed-off-by: Arnaud Pouliquen <arnaud.pouliquen@foss.st.com>
    Change-Id: I6dec7a51b622b44964f45c403648626efb1f1aba

--- drivers/remoteproc/stm32_rproc.c
+++ drivers/remoteproc/stm32_rproc.c
@@ -866,6 +866,15 @@ static int stm32_rproc_remove(struct platform_device *pdev)
 	return 0;
 }
 
+static void stm32_rproc_shutdown(struct platform_device *pdev)
+{
+	struct rproc *rproc = platform_get_drvdata(pdev);
+
+	if (atomic_read(&rproc->power) > 0)
+		dev_warn(&pdev->dev,
+			 "Warning: remote fw is still running with possible side effect!!!\n");
+}
+
 static int __maybe_unused stm32_rproc_suspend(struct device *dev)
 {
 	struct rproc *rproc = dev_get_drvdata(dev);
@@ -894,6 +903,7 @@ static SIMPLE_DEV_PM_OPS(stm32_rproc_pm_ops,
 static struct platform_driver stm32_rproc_driver = {
 	.probe = stm32_rproc_probe,
 	.remove = stm32_rproc_remove,
+	.shutdown = stm32_rproc_shutdown,
 	.driver = {
 		.name = "stm32-rproc",
 		.pm = &stm32_rproc_pm_ops,
