82c605c81f6b468c4d641b874e7a7fab46eef018
Pedro Perez de Heredia <pedro.perez@digi.com>
2020-06-09 21:14:42 +0200

    Revert "gpio-mca: fix interrupts on multiple instances"
    This reverts commit 02388dcbc85e25381e602fb5027005c9fc802f8b.

--- drivers/gpio/gpio-mca.c
+++ drivers/gpio/gpio-mca.c
@@ -68,7 +68,6 @@ struct mca_gpio {
 	struct regmap *regmap;
 	struct device *dev;
 	struct gpio_chip gc;
-	struct irq_chip irqchip;
 	struct mutex irq_lock;
 	uint8_t irq_cfg[MCA_MAX_IOS];
 	uint8_t irq_capable[MCA_MAX_IO_BYTES];
@@ -393,6 +392,16 @@ static int mca_gpio_to_irq(struct gpio_chip *gc, u32 offset)
 	return irq_create_mapping(gc->irq.domain, offset);
 }
 
+static struct irq_chip mca_gpio_irq_chip = {
+	.name			= "mca-gpio-irq",
+	.irq_disable		= mca_gpio_irq_disable,
+	.irq_enable		= mca_gpio_irq_enable,
+	.irq_bus_lock		= mca_gpio_irq_bus_lock,
+	.irq_bus_sync_unlock	= mca_gpio_irq_bus_sync_unlock,
+	.irq_set_type		= mca_gpio_irq_set_type,
+	.irq_set_wake		= mca_gpio_irq_set_wake,
+};
+
 static int mca_gpio_irq_setup(struct mca_gpio *gpio, int nbank)
 {
 	unsigned int val;
@@ -426,7 +435,7 @@ static int mca_gpio_irq_setup(struct mca_gpio *gpio, int nbank)
 		ret = devm_request_threaded_irq(gpio->dev, gpio->irq[i],
 						NULL, mca_gpio_irq_handler,
 						IRQF_TRIGGER_FALLING | IRQF_ONESHOT,
-						gpio->irqchip.name,
+						mca_gpio_irq_chip.name,
 						gpio);
 		if (ret) {
 			dev_err(gpio->dev, "Failed to request %s IRQ (%d)\n",
@@ -436,7 +445,7 @@ static int mca_gpio_irq_setup(struct mca_gpio *gpio, int nbank)
 	}
 
 	ret = gpiochip_irqchip_add_nested(&gpio->gc,
-					  &gpio->irqchip,
+					  &mca_gpio_irq_chip,
 					  0,
 					  handle_edge_irq,
 					  IRQ_TYPE_NONE);
@@ -456,7 +465,8 @@ static int mca_gpio_irq_setup(struct mca_gpio *gpio, int nbank)
 	for (i = 0; i < MCA_MAX_GPIO_IRQ_BANKS; i++) {
 		if (gpio->irq[i] < 0)
 			continue;
-		gpiochip_set_nested_irqchip(&gpio->gc, &gpio->irqchip,
+		gpiochip_set_nested_irqchip(&gpio->gc,
+					    &mca_gpio_irq_chip,
 					    gpio->irq[i]);
 	}
 
@@ -523,13 +533,6 @@ static int mca_gpio_probe(struct platform_device *pdev)
 	gpio->gc = reference_gc;
 	gpio->gc.of_node = pdev->dev.of_node;
 	gpio->gc.parent = &pdev->dev;
-	gpio->irqchip.name = "mca-gpio-irq",
-	gpio->irqchip.irq_disable = mca_gpio_irq_disable,
-	gpio->irqchip.irq_enable = mca_gpio_irq_enable,
-	gpio->irqchip.irq_bus_lock = mca_gpio_irq_bus_lock,
-	gpio->irqchip.irq_bus_sync_unlock = mca_gpio_irq_bus_sync_unlock,
-	gpio->irqchip.irq_set_type = mca_gpio_irq_set_type,
-	gpio->irqchip.irq_set_wake = mca_gpio_irq_set_wake,
 	platform_set_drvdata(pdev, gpio);
 
 	/* Find entry in device-tree */
