aed41fb369044ec709e6eb44397b6ea5671512ed
Ting-Ying Li <tingying.li@cypress.com>
2021-07-15 21:16:09 -0500

    brcmfmac: Add CYW89459 HW ID and modify sdio F2 block size/watermark
    - Add 4359d0 HW ID for supporting 89459 SDIO.
    - Modifying SDIO FUNC2 block size/watermark and mes watermark
    suggested by chip team.
    - Add more SDIO vendor and device ids for supporting CYW89459
    commercial products.
    
    Signed-off-by: Ting-Ying Li <tingying.li@infineon.com>

--- drivers/net/wireless/broadcom/brcm80211/include/brcm_hw_ids.h
+++ drivers/net/wireless/broadcom/brcm80211/include/brcm_hw_ids.h
@@ -61,6 +61,7 @@
 #define CY_CC_43439_CHIP_ID		43439
 #define CY_CC_43752_CHIP_ID		43752
 #define CY_CC_54591_CHIP_ID		0x4355
+#define CY_CC_89459_CHIP_ID		0x4355
 
 /* USB Device IDs */
 #define BRCM_USB_43143_DEVICE_ID	0xbd1e
