495df3b30d4d625bb2482a510aa9389fb6075775
Verma Avishad (IFINS CSS ICW ENG SW WFSW) <avishad.verma@infineon.com>
2022-09-30 12:36:44 +0000

    brcmfmac: Add 55500 chip support.
    Signed-off-by: Shelley Yang <shelley.yang@infineon.com>

--- drivers/net/wireless/broadcom/brcm80211/include/brcm_hw_ids.h
+++ drivers/net/wireless/broadcom/brcm80211/include/brcm_hw_ids.h
@@ -61,6 +61,7 @@
 #define CY_CC_43752_CHIP_ID		43752
 #define CY_CC_89459_CHIP_ID		0x4355
 #define CY_CC_55572_CHIP_ID		0xd908
+#define CY_CC_55500_CHIP_ID		0xD8CC
 
 /* USB Device IDs */
 #define BRCM_USB_43143_DEVICE_ID	0xbd1e
