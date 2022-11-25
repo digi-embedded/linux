1af6155c22cef5beb0b09b1de5aca38f70f5c1c4
avishad verma <avishad.verma@infineon.com>
2022-11-25 03:07:16 -0600

    brcmfmac: Update D2H Validation Done Timeout.
    Increasing the timeout to accomadate validation of Firmware with bigger
    size and RSA length.
    
    Signed-off-by: avishad verma <avishad.verma@infineon.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/chip.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/chip.c
@@ -259,8 +259,7 @@ struct sbsocramregs {
 #define BRCMF_BLHS_D2H_READY_TIMEOUT			100	/* msec */
 #define BRCMF_BLHS_D2H_TRXHDR_PARSE_DONE_TIMEOUT	50	/* msec */
 
-//TODO: original value is 450, check if need assign to specific chip or not.
-#define BRCMF_BLHS_D2H_VALDN_DONE_TIMEOUT		250	/* msec */
+#define BRCMF_BLHS_D2H_VALDN_DONE_TIMEOUT		450	/* msec */
 #define BRCMF_BLHS_D2H_MV_NVRAM_DONE_TIMEOUT		(100)	/* msec */
 #define BRCMF_BLHS_D2H_BP_CLK_DISABLE_REQ_TIMEOUT	(5 * 1000)	/* msec */
 
