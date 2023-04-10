a31651ce37c146b422a2adc05ed40b2da12b2cbf
Carter Chen <carter.chen@infineon.com>
2023-04-10 02:26:53 -0500

    non-upstream: sdio t-put tuning
    verifying on SDIO In-Band interrupt only as of present
    
    add module parameter "sdio_rxf_thread=1" to enable it.
    
    1.) Handling rx data frame of SDIO intf in a kthread.
    2.) fixing a potential null pointer dereference in sdio_isr
        which dues to a data structure is not ready.
    
    Fixes: CYW55560A1-1343
    
    Signed-off-by: Carter Chen <carter.chen@infineon.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.h
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.h
@@ -10,6 +10,13 @@
 #include <linux/firmware.h>
 #include "firmware.h"
 
+#if (LINUX_VERSION_CODE <= KERNEL_VERSION(6, 6, 15))
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0))
+#include <uapi/linux/sched/types.h>
+#endif /* kernel 4.11.0 */
+#endif /* kernel 6.6.15 */
+
+
 #define SDIOD_FBR_SIZE		0x100
 
 /* io_en */
