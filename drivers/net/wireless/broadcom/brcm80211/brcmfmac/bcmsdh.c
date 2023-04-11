5033d27022fbb42e73f724a6ccee332a5d16400a
Giridharan A V <gias@cypress.com>
2023-04-11 03:26:45 -0500

    brcmfmac: fixes for FW redownload after DS1/2 exit
    Fixes for multiple issues seen during FW redownload after DS1/2 exit
    
    1. Stop the WD timer when entering the re-download path
       to avoid attempting to the bus.
    2. Reset the "last" index when the console buffer is updated.
    3. Do not read the console buffer right after downloading
       the FW as the shared structure might be populated by then.
    4. Post download, signature of the FW is verified.
       It is in the order of ~400-500ms.
       So, wait for ~500ms in the FMAC for the FW to be ready post download.
    5. FW download process moves even if the post NVRAM download check fails.
       So, stop proceeding with FW download if post NVRAM download checks fail.
    6. Add some more debug prints.
    7. Do not acknowledge the HMB data read to FW when HMB data is zero.
    
    Signed-off-by: Mahankali Chandra Sekhar <chandrasekhar.mahankali@infineon.com>
    Signed-off-by: Giridharan A V <gias@cypress.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/bcmsdh.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/bcmsdh.c
@@ -257,12 +257,14 @@ u32 brcmf_sdiod_readl(struct brcmf_sdio_dev *sdiodev, u32 addr, int *ret)
 	if (retval)
 		goto out;
 
-	brcmf_dbg(TRACE, "reading from addr 0x%x bar0 0x%08x\n", addr, sdiodev->sbwad);
+	brcmf_dbg(TRACE, "reading from addr 0x%x bar0 0x%08x ", addr, sdiodev->sbwad);
 
 	addr &= SBSDIO_SB_OFT_ADDR_MASK;
 	addr |= SBSDIO_SB_ACCESS_2_4B_FLAG;
 
 	data = sdio_readl(sdiodev->func1, addr, &retval);
+
+	brcmf_dbg(TRACE, "data 0x%08x\n", data);
 out:
 	if (ret)
 		*ret = retval;
@@ -279,7 +281,7 @@ void brcmf_sdiod_writel(struct brcmf_sdio_dev *sdiodev, u32 addr,
 	if (retval)
 		goto out;
 
-	brcmf_dbg(TRACE, "writing %d to addr 0x%x bar0 0x%08x\n", data, addr, sdiodev->sbwad);
+	brcmf_dbg(TRACE, "writing 0x%08x to addr 0x%x bar0 0x%08x\n", data, addr, sdiodev->sbwad);
 
 	addr &= SBSDIO_SB_OFT_ADDR_MASK;
 	addr |= SBSDIO_SB_ACCESS_2_4B_FLAG;
