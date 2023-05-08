df2947b95b584c70169e7c54b152dd2535834309
P Rakshith (IFINS CAE 1 A) <rakshith.p@infineon.com>
2023-05-08 06:58:14 +0000

    brcmfmac: Add support for ethtool packet statistics.
    Signed-off-by: Rakshith P <rakshith.p@infineon.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/core.h
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/core.h
@@ -148,6 +148,8 @@ struct brcmf_pub {
 	const struct brcmf_fwvid_ops *vops;
 	void *vdata;
 	struct brcmf_pkt_filter_enable_le pkt_filter[MAX_PKT_FILTER_COUNT];
+	u16 cnt_ver;
+
 	struct cfg80211_qos_map *qos_map;
 };
 
