eeb0345919bbe0fd2cb4e7d22bf6585dc571005b
Stephane DANIEAU <stephane.danieau@st.com>
2023-06-07 16:23:55 +0200

    pinctrl: stm32: add tristate option for stm32mp257
    Add ability to build pinctrl for stm32mp257 as a module.
    
    Signed-off-by: Stephane DANIEAU <stephane.danieau@st.com>
    Change-Id: I0e98e083b14bc95e5653aeea0d0a9adf0e120cbd
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/linux-stm32/+/321298
    Tested-by: Antonio Maria BORNEO <antonio.borneo@foss.st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: Antonio Maria BORNEO <antonio.borneo@foss.st.com>
    Reviewed-by: Fabrice GASNIER <fabrice.gasnier@foss.st.com>
    Domain-Review: Fabrice GASNIER <fabrice.gasnier@foss.st.com>
    ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>

--- drivers/pinctrl/stm32/pinctrl-stm32.c
+++ drivers/pinctrl/stm32/pinctrl-stm32.c
@@ -1983,6 +1983,7 @@ int stm32_pctl_probe(struct platform_device *pdev)
 
 	return 0;
 }
+EXPORT_SYMBOL(stm32_pctl_probe);
 
 static int __maybe_unused stm32_pinctrl_restore_advcfgr(struct stm32_gpio_bank *bank,
 							int offset, u32 bpos)
@@ -2095,6 +2096,7 @@ int __maybe_unused stm32_pinctrl_suspend(struct device *dev)
 
 	return 0;
 }
+EXPORT_SYMBOL(stm32_pinctrl_suspend);
 
 int __maybe_unused stm32_pinctrl_resume(struct device *dev)
 {
@@ -2110,3 +2112,7 @@ int __maybe_unused stm32_pinctrl_resume(struct device *dev)
 
 	return 0;
 }
+EXPORT_SYMBOL(stm32_pinctrl_resume);
+
+MODULE_DESCRIPTION("STM32 core pinctrl driver");
+MODULE_LICENSE("GPL");
