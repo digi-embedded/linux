de198b49f9928e9d930d035c77c16238edd176ae
Ishan Deshpande <IshanShirish.Deshpande@infineon.com>
2023-09-22 09:11:52 -0500

    brcmfmac: Check bus sleep state in isr
    Check if the bus is in sleep state in interrupt service routine before
    reading the interrupt status register. Wake up the bus if it is in sleep
    state as accessing the backplane when bus in sleep may lead to a crash
    
    Signed-off-by: Ishan Deshpande <IshanShirish.Deshpande@infineon.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.c
@@ -1236,6 +1236,11 @@ done:
 
 }
 
+bool brcmf_sdio_bus_sleep_state(struct brcmf_sdio *bus)
+{
+	return bus->sleeping;
+}
+
 #ifdef DEBUG
 static inline bool brcmf_sdio_valid_shared_address(u32 addr)
 {
@@ -4417,6 +4422,11 @@ void brcmf_sdio_isr(struct brcmf_sdio *bus, bool in_isr)
 	if (!bus) {
 		brcmf_err("bus is null pointer, exiting\n");
 		return;
+	}
+
+	/* Wake up the bus if in sleep */
+	if (brcmf_sdio_bus_sleep_state(bus)) {
+		brcmf_sdio_bus_sleep(bus, false, false);
 	}
 
 	/* Count the interrupt call */
