4f964cef1ab182e731ce611dbc876b5271068ef9
Hsu Wyatt (CSS ICW SW WFSW 2) <wyatt.hsu@infineon.com>
2023-10-17 06:01:42 +0000

    brcmfmac: offload: update the brcmf_configure_arp_nd_offload

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/core.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/core.c
@@ -263,30 +263,44 @@ void brcmf_configure_arp_nd_offload(struct brcmf_if *ifp, bool enable)
 	else
 		mode = 0;
 
-	/* Try to set and enable ARP offload feature, this may fail, then it  */
-	/* is simply not supported and err 0 will be returned                 */
-	err = brcmf_fil_iovar_int_set(ifp, "arp_ol", mode);
-	if (err) {
-		brcmf_dbg(TRACE, "failed to set ARP offload mode to 0x%x, err = %d\n",
-			  mode, err);
+	if (brcmf_feat_is_enabled(ifp, BRCMF_FEAT_OFFLOADS)) {
+		u32 feat_set = brcmf_offload_feat & (BRCMF_OL_ARP | BRCMF_OL_ND);
+
+		if (!feat_set)
+			return;
+
+		if (enable)
+			brcmf_generic_offload_config(ifp, feat_set,
+						     brcmf_offload_prof, false);
+		else
+			brcmf_generic_offload_config(ifp, feat_set,
+						     brcmf_offload_prof, true);
 	} else {
-		err = brcmf_fil_iovar_int_set(ifp, "arpoe", enable);
+		/* Try to set and enable ARP offload feature, this may fail, then it  */
+		/* is simply not supported and err 0 will be returned                 */
+		err = brcmf_fil_iovar_int_set(ifp, "arp_ol", mode);
 		if (err) {
-			brcmf_dbg(TRACE, "failed to configure (%d) ARP offload err = %d\n",
+			brcmf_dbg(TRACE, "failed to set ARP offload mode to 0x%x, err = %d\n",
+				  mode, err);
+		} else {
+			err = brcmf_fil_iovar_int_set(ifp, "arpoe", enable);
+			if (err) {
+				brcmf_dbg(TRACE, "failed to configure (%d) ARP offload err = %d\n",
+					  enable, err);
+			} else {
+				brcmf_dbg(TRACE, "successfully configured (%d) ARP offload to 0x%x\n",
+					  enable, mode);
+			}
+		}
+
+		err = brcmf_fil_iovar_int_set(ifp, "ndoe", enable);
+		if (err) {
+			brcmf_dbg(TRACE, "failed to configure (%d) ND offload err = %d\n",
 				  enable, err);
 		} else {
-			brcmf_dbg(TRACE, "successfully configured (%d) ARP offload to 0x%x\n",
+			brcmf_dbg(TRACE, "successfully configured (%d) ND offload to 0x%x\n",
 				  enable, mode);
 		}
-	}
-
-	err = brcmf_fil_iovar_int_set(ifp, "ndoe", enable);
-	if (err) {
-		brcmf_dbg(TRACE, "failed to configure (%d) ND offload err = %d\n",
-			  enable, err);
-	} else {
-		brcmf_dbg(TRACE, "successfully configured (%d) ND offload to 0x%x\n",
-			  enable, mode);
 	}
 }
 
