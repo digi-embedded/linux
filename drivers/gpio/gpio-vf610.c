49c4e4def9175a042a20bd0763a3c296d6e304d8
Hector Palacios <hector.palacios@digi.com>
2024-01-11 12:12:36 +0100

    gpio: vf610: add support to DT 'ngpios' property
    Some SoCs, such as i.MX93, don't have all 32 pins available
    per port. Allow optional generic 'ngpios' property to be
    specified from the device tree and default to
    VF610_GPIO_PER_PORT (32) if the property does not exist.
    
    Signed-off-by: Hector Palacios <hector.palacios@digi.com>

--- drivers/gpio/gpio-vf610.c
+++ drivers/gpio/gpio-vf610.c
@@ -263,6 +263,7 @@ static int vf610_gpio_probe(struct platform_device *pdev)
 	struct vf610_gpio_port *port;
 	struct gpio_chip *gc;
 	struct gpio_irq_chip *girq;
+	u32 ngpios;
 	int i;
 	int ret;
 
@@ -318,7 +319,11 @@ static int vf610_gpio_probe(struct platform_device *pdev)
 	gc = &port->gc;
 	gc->parent = dev;
 	gc->label = dev_name(dev);
-	gc->ngpio = VF610_GPIO_PER_PORT;
+	ret = device_property_read_u32(dev, "ngpios", &ngpios);
+	if (ret || ngpios > VF610_GPIO_PER_PORT)
+		gc->ngpio = VF610_GPIO_PER_PORT;
+	else
+		gc->ngpio = (u16)ngpios;
 	gc->base = of_alias_get_id(np, "gpio") * VF610_GPIO_PER_PORT;
 
 	gc->request = gpiochip_generic_request;
