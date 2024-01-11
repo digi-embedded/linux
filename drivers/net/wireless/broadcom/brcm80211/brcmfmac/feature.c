5cb9e31b7588b98e98b4c13261949b9c773db4e5
Ethan Kim <kims@cypress.com>
2024-01-11 16:22:20 -0600

    brcmfmac: Add new survey dump feature.
     Added brcmf_cfg80211_dump_survey_2 function for ops->dump_survey
     This is informed for survey information flags
    
     Signed-off-by: Ethan Kim ethan.kim@infineon.com

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/feature.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/feature.c
@@ -361,6 +361,7 @@ void brcmf_feat_attach(struct brcmf_pub *drvr)
 	brcmf_feat_iovar_int_get(ifp, BRCMF_FEAT_TDLS, "tdls_enable");
 	brcmf_feat_iovar_int_get(ifp, BRCMF_FEAT_MFP, "mfp");
 	brcmf_feat_iovar_int_get(ifp, BRCMF_FEAT_DUMP_OBSS, "dump_obss");
+	brcmf_feat_iovar_int_get(ifp, BRCMF_FEAT_SURVEY_DUMP, "cca_survey_dump");
 
 	pfn_mac.version = BRCMF_PFN_MACADDR_CFG_VER;
 	err = brcmf_fil_iovar_data_get(ifp, "pfn_macaddr", &pfn_mac,
