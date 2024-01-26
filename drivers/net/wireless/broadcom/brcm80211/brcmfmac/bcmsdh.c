a9ef4d22164fb2c7d1ad351d8073819a802d6e7e
Ian Lin <ian.lin@infineon.com>
2024-01-25 19:41:27 -0600

    brcmfmac: separate 43022 sdio download function from common flow
    After accommodate 43022 flow, the common flow become wrong
    for some other chips, thus separate them to two function
    
    Signed-off-by: Ian Lin <ian.lin@infineon.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/bcmsdh.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/bcmsdh.c
@@ -257,14 +257,14 @@ u32 brcmf_sdiod_readl(struct brcmf_sdio_dev *sdiodev, u32 addr, int *ret)
 	if (retval)
 		goto out;
 
-	brcmf_dbg(TRACE, "reading from addr 0x%x bar0 0x%08x ", addr, sdiodev->sbwad);
+	brcmf_dbg(SDIO, "reading from addr 0x%x bar0 0x%08x ", addr, sdiodev->sbwad);
 
 	addr &= SBSDIO_SB_OFT_ADDR_MASK;
 	addr |= SBSDIO_SB_ACCESS_2_4B_FLAG;
 
 	data = sdio_readl(sdiodev->func1, addr, &retval);
 
-	brcmf_dbg(TRACE, "data 0x%08x\n", data);
+	brcmf_dbg(SDIO, "data 0x%08x\n", data);
 out:
 	if (ret)
 		*ret = retval;
@@ -281,7 +281,7 @@ void brcmf_sdiod_writel(struct brcmf_sdio_dev *sdiodev, u32 addr,
 	if (retval)
 		goto out;
 
-	brcmf_dbg(TRACE, "writing 0x%08x to addr 0x%x bar0 0x%08x\n", data, addr, sdiodev->sbwad);
+	brcmf_dbg(SDIO, "writing 0x%08x to addr 0x%x bar0 0x%08x\n", data, addr, sdiodev->sbwad);
 
 	addr &= SBSDIO_SB_OFT_ADDR_MASK;
 	addr |= SBSDIO_SB_ACCESS_2_4B_FLAG;
