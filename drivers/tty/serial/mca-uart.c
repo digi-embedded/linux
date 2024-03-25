917a20a762dd0212c1d74983c1fba3f5564b59c6
Isaac Hermida <isaac.hermida@digi.com>
2024-03-25 16:23:28 +0100

    mca: remove devm_gpio_free() calls
    devm_gpio_free() is not needed anymore. Remove dead code
    
    https://onedigi.atlassian.net/browse/DEL-8967
    
    Signed-off-by: Isaac Hermida <isaac.hermida@digi.com>

--- drivers/tty/serial/mca-uart.c
+++ drivers/tty/serial/mca-uart.c
@@ -935,19 +935,9 @@ static int mca_uart_allocate_port_resources(struct mca_uart_drv *uart_drv,
 					NULL, mca_uart_irq_handler,
 					IRQF_ONESHOT, MCA_BASE_DRVNAME_UART,
 					mca_uart);
-	if (ret) {
+	if (ret)
 		dev_err(uart_drv->dev, "Failed to register IRQ\n");
-		goto error_ios;
-	}
 
-	return 0;
-
-error_ios:
-	for (i = 0; i < mca_uart->npins; i++) {
-		int io = mca_uart->pins[i];
-
-		devm_gpio_free(uart_drv->dev, uart_drv->mca->gpio_base + io);
-	}
 	return ret;
 }
 
@@ -955,7 +945,6 @@ static int mca_uart_release_port_resources(struct mca_uart *mca_uart)
 {
 	struct mca_uart_drv *uart_drv =
 		dev_get_drvdata(mca_uart->port.dev->parent);
-	int i;
 
 	cancel_work_sync(&mca_uart->tx_work);
 	cancel_work_sync(&mca_uart->dc_work);
@@ -965,12 +954,6 @@ static int mca_uart_release_port_resources(struct mca_uart *mca_uart)
 
 	if (mca_uart->port.irq)
 		devm_free_irq(uart_drv->dev, mca_uart->port.irq, mca_uart);
-
-	for (i = 0; i < mca_uart->npins; i++) {
-		int io = mca_uart->pins[i];
-
-		devm_gpio_free(uart_drv->dev, uart_drv->mca->gpio_base + io);
-	}
 
 	return 0;
 }
