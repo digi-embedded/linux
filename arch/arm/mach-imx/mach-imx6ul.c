72a803834c876f6a0144c74e9787c52105d218c6
Isaac Hermida <isaac.hermida@digi.com>
2024-05-07 13:07:04 +0200

    mach-imx6ul: remove dead code
    The xbee reset cannot be run at this state, as the gpio node is not probed
    yet in the system, so you get an EPROBE_DEFER.
    
    This error happened as well in previous kernel versions, but there was no
    code there printing the failing situation.
    
    The XBee Reset is done in the specific rootfs script that initializes the XBee
    node.
    
    https://onedigi.atlassian.net/browse/DEL-9019
    
    Signed-off-by: Isaac Hermida <isaac.hermida@digi.com>

--- arch/arm/mach-imx/mach-imx6ul.c
+++ arch/arm/mach-imx/mach-imx6ul.c
@@ -32,40 +32,12 @@ static void __init imx6ul_init_irq(void)
 	imx6_pm_ccm_init("fsl,imx6ul-ccm");
 }
 
-static void __init imx6ul_xbee_init(void)
-{
-	struct device_node *np;
-	struct gpio_desc *reset_gpio;
-
-	np = of_find_node_by_path("/xbee");
-	if (!np)
-		return;
-
-	/* Read the XBee reset gpio */
-	reset_gpio = fwnode_gpiod_get_index(of_fwnode_handle(np), "digi,reset", 0,
-					   GPIOD_OUT_LOW, "xbee-reset");
-	if (IS_ERR(reset_gpio)) {
-		pr_warn("failed to get xbee-reset gpio\n");
-		reset_gpio = NULL;
-	}
-	if (reset_gpio) {
-		gpiod_set_value_cansleep(reset_gpio, 1);
-		mdelay(1);
-		gpiod_set_value_cansleep(reset_gpio, 0);
-		gpiod_put(reset_gpio);
-	}
-
-	of_node_put(np);
-}
-
 static void __init imx6ul_init_late(void)
 {
 	imx6ul_cpuidle_init();
 
 	if (IS_ENABLED(CONFIG_ARM_IMX6Q_CPUFREQ))
 		platform_device_register_simple("imx6q-cpufreq", -1, NULL, 0);
-
-	imx6ul_xbee_init();
 }
 
 static void __init imx6ul_map_io(void)
