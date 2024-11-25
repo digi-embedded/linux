9a571ab05662466735138c22c2b3522e228a02ae
Arturo Buzarra <arturo.buzarra@digi.com>
2024-11-25 21:49:17 +0100

    drm/bridge: lt8912: remove i2c_device_id * parameter from probe function
    After commit 03c835f498b5 ("i2c: Switch .probe() to not take an id parameter")
    the probe function doesn't make use of the i2c_device_id * parameter so it must
    to be removed to avoid a build issue.
    
    https://onedigi.atlassian.net/browse/DEL-9296
    
    Signed-off-by: Arturo Buzarra <arturo.buzarra@digi.com>

--- drivers/gpu/drm/bridge/lt8912.c
+++ drivers/gpu/drm/bridge/lt8912.c
@@ -594,7 +594,7 @@ void lt8912_detach_dsi(struct lt8912 *lt)
 }
 
 
-static int lt8912_probe(struct i2c_client *i2c, const struct i2c_device_id *id)
+static int lt8912_probe(struct i2c_client *i2c)
 {
 	struct device *dev = &i2c->dev;
 	struct lt8912 *lt;
