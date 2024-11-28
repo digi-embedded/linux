e445814d11b8c7520a15035387b511ff2da0c0a2
Alain Volmat <alain.volmat@foss.st.com>
2024-11-28 10:16:25 +0100

    media: stm32: dcmipp: set V4L2_SUBDEV_FL_STREAMS for dcmipp-input
    dcmipp-input must use the stream apis in order to be able to properly
    perform the selection of input data and perform the routing into pipes.
    
    Change-Id: I87874b840b56957328e55f186c6ee8ce0db72893
    Signed-off-by: Alain Volmat <alain.volmat@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/linux-stm32/+/499331
    ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>
    Reviewed-by: Alain VOLMAT <alain.volmat@st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Tested-by: Alain VOLMAT <alain.volmat@st.com>
    Reviewed-by: Hugues FRUCHET <hugues.fruchet@st.com>
    Reviewed-by: Philippe CORNU <philippe.cornu@foss.st.com>
    Domain-Review: Philippe CORNU <philippe.cornu@foss.st.com>

--- drivers/media/platform/st/stm32/stm32-dcmipp/dcmipp-common.c
+++ drivers/media/platform/st/stm32/stm32-dcmipp/dcmipp-common.c
@@ -72,6 +72,9 @@ int dcmipp_ent_sd_register(struct dcmipp_ent_device *ved,
 	sd->flags |= V4L2_SUBDEV_FL_HAS_DEVNODE;
 	if (sd->ctrl_handler)
 		sd->flags |= V4L2_SUBDEV_FL_HAS_EVENTS;
+	/* dcmipp_input deals with streams to perform proper routing */
+	if (!strcmp(name, "dcmipp_input"))
+		sd->flags |= V4L2_SUBDEV_FL_STREAMS;
 
 	/* Initialize the media entity */
 	ret = media_entity_pads_init(&sd->entity, num_pads, ved->pads);
