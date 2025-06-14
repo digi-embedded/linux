4a2d75223fb38f3e23505468b0989f5eea763454
Owen Huang <Owen.Huang@infineon.com>
2025-06-14 13:10:24 -0500

    non-upstream: cleanup fws in link down while SDIO bus error happened
    Some FW signal information might not correct while SDIO bus error
    happened, so apply a workaround in link down to cleanup related queue
    and variables.
    
    Fixes SWWLAN-153899
    
    Signed-off-by: Owen Huang <Owen.Huang@infineon.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.c
@@ -8270,6 +8270,7 @@ brcmf_notify_connect_status(struct brcmf_if *ifp,
 				complete(&cfg->vif_disabled);
 			brcmf_net_setcarrier(ifp, false);
 		}
+		brcmf_fws_wa_cleanup_by_ifidx(ifp->drvr, ifp->ifidx);
 	} else if (brcmf_is_nonetwork(cfg, e)) {
 		if (brcmf_is_ibssmode(ifp->vif))
 			clear_bit(BRCMF_VIF_STATUS_CONNECTING,
