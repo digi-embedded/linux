wmamills/linux-openamp-staging

commit f2c398cb189f726806a167cd04ea42837811db7e

usb: dwc3-stm32: Add pm_runtime support
Add pm_runtime enable and pm_runtime_resume_and_get in probe function.
This will prevent the platform to automatically enter low power mode
at runtime, when PSCI OSI (S2Idle) feature will be enabled.

Change-Id: I561fae26c2d1ad2052c6987f15b6648e4080fd82
Signed-off-by: Rahul Kumar <rahul.kumar05@st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/linux-stm32/+/403010
Reviewed-by: Fabrice GASNIER <fabrice.gasnier@foss.st.com>
Domain-Review: Fabrice GASNIER <fabrice.gasnier@foss.st.com>
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>

drivers/usb/dwc3/dwc3-stm32.c
@@ -195,6 +195,14 @@ static int stm32_dwc3_probe(struct platform_device *pdev)
 	if (ret)
 		return dev_err_probe(dev, ret, "failed to add dwc3 core\n");
 
+	ret = devm_pm_runtime_enable(dev);
+	if (ret)
+		return dev_err_probe(dev, ret, "Failed to enable pm runtime\n");
+
+	ret = pm_runtime_resume_and_get(dev);
+	if (ret)
+		return dev_err_probe(dev, ret, "pm runtime resume failed\n");
+
 	platform_set_drvdata(pdev, dwc3_data);
 
 	if (wakeup_source)

digi-embedded/linux

commit d30466e9256a0569e4823cccac1ea5696ea31ce9

usb: dwc3-stm32: Activate pm_runtime before enabling it
With current implementation, PM runtime enable generates a warning:
[   19.602987] usb-stm32-dwc3 42080000.bus:usb@48300000: Enabling runtime PM for inactive device with an active children

It is due to the fact that PM runtime has not been enabled before
populating dwc3 children (which enables it).
So fix the initialization sequence: Activate pm_runtime with
pm_runtime_set_active(), then enable it before populating children.

Fixes: f2c398cb189f ("usb: dwc3-stm32: Add pm_runtime support")
Change-Id: Ieaf17565e2bb347da2a0c4f62339a6c93265ed5d
Signed-off-by: Amelie Delaunay <amelie.delaunay@foss.st.com>
Signed-off-by: Fabrice Gasnier <fabrice.gasnier@foss.st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/linux-stm32/+/451589
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>

drivers/usb/dwc3/dwc3-stm32.c
@@ -190,18 +190,18 @@ static int stm32_dwc3_probe(struct platform_device *pdev)
 		device_set_wakeup_capable(dev, true);
 	}
 
-	/* Allocate and initialize the core */
-	ret = devm_of_platform_populate(dev);
+	ret = pm_runtime_set_active(dev);
 	if (ret)
-		return dev_err_probe(dev, ret, "failed to add dwc3 core\n");
+		return dev_err_probe(dev, ret, "Failed to activate pm runtime\n");
 
 	ret = devm_pm_runtime_enable(dev);
 	if (ret)
 		return dev_err_probe(dev, ret, "Failed to enable pm runtime\n");
 
-	ret = pm_runtime_resume_and_get(dev);
+	/* Allocate and initialize the core */
+	ret = devm_of_platform_populate(dev);
 	if (ret)
-		return dev_err_probe(dev, ret, "pm runtime resume failed\n");
+		return dev_err_probe(dev, ret, "failed to add dwc3 core\n");
 
 	platform_set_drvdata(pdev, dwc3_data);
 