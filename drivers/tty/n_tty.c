eaf5814c1eae54041c4e819708ee4d4eaa9f0102
Alain Volmat <alain.volmat@foss.st.com>
2025-08-05 12:32:25 +0200

    Reapply "tty: Use flow-control char function on closing path"
    Put back the original commit now that tty instability is understood and
    not related to this commit.
    
    This reverts commit edfa27a80fdb5ecb7b0df88e51353723d3fd81c6.
    
    Change-Id: Ib237744e384686815240e28419848f3352552ea4
    Signed-off-by: Alain Volmat <alain.volmat@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/linux-stm32/+/481043
    Domain-Review: Amelie DELAUNAY <amelie.delaunay@foss.st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>
    Tested-by: Alain VOLMAT <alain.volmat@st.com>
    Reviewed-by: Alain VOLMAT <alain.volmat@st.com>
    Reviewed-by: Amelie DELAUNAY <amelie.delaunay@foss.st.com>

--- drivers/tty/n_tty.c
+++ drivers/tty/n_tty.c
@@ -1457,15 +1457,10 @@ static void n_tty_receive_char_closing(struct tty_struct *tty, u8 c,
 		c = tolower(c);
 
 	if (I_IXON(tty)) {
-		if (c == STOP_CHAR(tty)) {
-			if (!lookahead_done)
-				stop_tty(tty);
-		} else if (c == START_CHAR(tty) && lookahead_done) {
-			return;
-		} else if (c == START_CHAR(tty) ||
-			 (tty->flow.stopped && !tty->flow.tco_stopped && I_IXANY(tty) &&
-			  c != INTR_CHAR(tty) && c != QUIT_CHAR(tty) &&
-			  c != SUSP_CHAR(tty))) {
+		if (!n_tty_receive_char_flow_ctrl(tty, c, lookahead_done) &&
+		    tty->flow.stopped && !tty->flow.tco_stopped && I_IXANY(tty) &&
+		    c != INTR_CHAR(tty) && c != QUIT_CHAR(tty) &&
+		    c != SUSP_CHAR(tty)) {
 			start_tty(tty);
 			process_echoes(tty);
 		}
