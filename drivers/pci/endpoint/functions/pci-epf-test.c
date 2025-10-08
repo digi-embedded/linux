d618d615bcddb958252196e338dc7d73221737c4
Christian Bruel <christian.bruel@foss.st.com>
2025-10-08 11:52:01 +0200

    PCI: endpoint: Introduce 'epc_deinit' event and notify the EPF drivers
    partial backport of:
    [ Upstream commit 473b2cf9c4d1711146da1d8574c61e92c6672892 ]
    
    As like the 'epc_init' event, that is used to signal the EPF drivers about
    the EPC initialization, let's introduce 'epc_deinit' event that is used to
    signal EPC deinitialization.
    
    The EPC deinitialization applies only when any sort of fundamental reset
    is supported by the endpoint controller as per the PCIe spec.
    
    Reference: PCIe r6.0, sec 4.2.5.9.1 and 6.6.1.
    
    Currently, some EPC drivers like pcie-qcom-ep and pcie-tegra194 support
    PERST# as the fundamental reset. So the 'deinit' event will be notified to
    the EPF drivers when PERST# assert happens in the above mentioned EPC
    drivers.
    
    The EPF drivers, on receiving the event through the epc_deinit() callback
    should reset the EPF state machine and also cleanup any configuration that
    got affected by the fundamental reset like BAR, DMA etc...
    
    This change also warrants skipping the cleanups in unbind() if already done
    in epc_deinit().
    
    Link: https://lore.kernel.org/r/20240606-pci-deinit-v1-2-4395534520dc@linaro.org
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    Signed-off-by: Krzysztof Wilczyński <kwilczynski@kernel.org>
    Reviewed-by: Niklas Cassel <cassel@kernel.org>
    Reviewed-by: Siddharth Vadapalli <s-vadapalli@ti.com>
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Signed-off-by: Christian Bruel <christian.bruel@foss.st.com>
    [Use core_deinit instead of epc_deinit for v6.6 notifiers callback mechanism]
    Change-Id: Ia6a4a4845e9bb26cae02e6dab7aebccda48cb7b9
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/linux-stm32/+/497597
    Reviewed-by: Fabrice GASNIER <fabrice.gasnier@foss.st.com>
    Domain-Review: Fabrice GASNIER <fabrice.gasnier@foss.st.com>

--- drivers/pci/endpoint/functions/pci-epf-test.c
+++ drivers/pci/endpoint/functions/pci-epf-test.c
@@ -738,6 +738,15 @@ static void pci_epf_test_clear_bar(struct pci_epf *epf)
 	}
 }
 
+static void pci_epf_test_core_deinit(struct pci_epf *epf)
+{
+	struct pci_epf_test *epf_test = epf_get_drvdata(epf);
+
+	cancel_delayed_work_sync(&epf_test->cmd_handler);
+	pci_epf_test_clean_dma_chan(epf_test);
+	pci_epf_test_clear_bar(epf);
+}
+
 static int pci_epf_test_core_init(struct pci_epf *epf)
 {
 	struct pci_epf_test *epf_test = epf_get_drvdata(epf);
@@ -802,6 +811,7 @@ static int pci_epf_test_link_up(struct pci_epf *epf)
 
 static const struct pci_epc_event_ops pci_epf_test_event_ops = {
 	.core_init = pci_epf_test_core_init,
+	.core_deinit = pci_epf_test_core_deinit,
 	.link_up = pci_epf_test_link_up,
 };
 
