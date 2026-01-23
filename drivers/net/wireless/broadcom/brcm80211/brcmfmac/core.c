Infineon/ifx-wireless-drivers

non-upstream: btsdio: add command to show bt shared sdio information
Command: cat /sys/kernel/debug/ieee80211/phy0/bts_info

Fixes SWWLAN-147955

Signed-off-by: Owen Huang <Owen.Huang@infineon.com>

drivers/net/wireless/broadcom/brcm80211/brcmfmac/core.c
@@ -30,6 +30,7 @@
 #include "pcie.h"
 #include "common.h"
 #include "twt.h"
+#include "bt_shared_sdio_ifx.h"
 
 #define MAX_WAIT_FOR_8021X_TX			msecs_to_jiffies(950)
 
@@ -1658,6 +1659,7 @@ static int brcmf_bus_started(struct brcmf_pub *drvr, struct cfg80211_ops *ops)
 	brcmf_proto_debugfs_create(drvr);
 	brcmf_bus_debugfs_create(bus_if);
 	brcmf_twt_debugfs_create(drvr);
+	ifx_btsdio_debugfs_create(drvr);
 
 	return 0;
 

digi-embedded/linux

drivers/net/wireless/broadcom/brcm80211/brcmfmac/core.c
@@ -32,6 +32,7 @@
 #include "pcie.h"
 #include "common.h"
 #include "twt.h"
+#include "bt_shared_sdio_ifx.h"
 
 #define MAX_WAIT_FOR_8021X_TX			msecs_to_jiffies(950)
 
@@ -1659,6 +1660,7 @@ static int brcmf_bus_started(struct brcmf_pub *drvr, struct cfg80211_ops *ops)
 	brcmf_proto_debugfs_create(drvr);
 	brcmf_bus_debugfs_create(bus_if);
 	brcmf_twt_debugfs_create(drvr);
+	inf_btsdio_debugfs_create(drvr);
 
 	return 0;
 