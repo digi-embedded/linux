Infineon/ifx-wireless-drivers

non-upstream: btsdio: check rx interrupt before get rx header
1. If wi-fi get the zero length in rx header and bt access F3
at the same time, sdio will hang and cannot work. So driver should
check interrupt to know Rx FIFO is empty or not instead of read rx
header.

2. Make sure bt active checking and wlan rx header in the same
critical section.

Fixes 43012C1-210, HW55500-1080, CYHW43012-77

Signed-off-by: Owen Huang <Owen.Huang@infineon.com>

(cherry picked from commit 7bdae5e8e3074ac2958ae776557e6fbcd3fea6e0)

drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.c
@@ -2176,6 +2176,31 @@ static void brcmf_sdio_pad(struct brcmf_sdio *bus, u16 *pad, u16 *rdlen)
 	}
 }
 
+static bool brcmf_sdio_rx_pkt_is_avail(struct brcmf_sdio *bus)
+{
+	struct brcmf_sdio_dev *sdiod = bus->sdiodev;
+	u32 newstatus = 0;
+	u32 intstat_addr = bus->sdio_core->base + SD_REG(intstatus);
+	int err = 0;
+	bool ret = true;
+
+	if (!ifx_btsdio_is_active(bus->sdiodev->bus_if))
+		return true;
+
+	/* read interrupt to get fifo status*/
+	newstatus = brcmf_sdiod_readl(sdiod, intstat_addr, &err);
+	if (err < 0) {
+		brcmf_err("read int status failed %d\n", err);
+		ret = false;
+	} else if (!(newstatus & I_XMTDATA_AVAIL)) {
+		/* no more frames */
+		brcmf_dbg(DATA, "no more frames, int status: 0x%08x\n", newstatus);
+		ret = false;
+	}
+
+	return ret;
+}
+
 static uint brcmf_sdio_readframes(struct brcmf_sdio *bus, uint maxframes)
 {
 	struct sk_buff *pkt;		/* Packet for event or data frames */
@@ -2209,9 +2234,16 @@ static uint brcmf_sdio_readframes(struct brcmf_sdio *bus, uint maxframes)
 			continue;
 		}
 
+		sdio_claim_host(bus->sdiodev->func1);
+		/* make sure rx pkt check and rece buf in the same critical section */
+		if (!brcmf_sdio_rx_pkt_is_avail(bus)) {
+			bus->rxpending = false;
+			sdio_release_host(bus->sdiodev->func1);
+			break;
+		}
+
 		rd->len_left = rd->len;
 		/* read header first for unknow frame length */
-		sdio_claim_host(bus->sdiodev->func1);
 		if (!rd->len) {
 			ret = brcmf_sdiod_recv_buf(bus->sdiodev, SDIO_FUNC_2,
 						   bus->rxhdr, BRCMF_FIRSTREAD);

digi-embedded/linux

drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.c
@@ -2177,6 +2177,31 @@ static void brcmf_sdio_pad(struct brcmf_sdio *bus, u16 *pad, u16 *rdlen)
 	}
 }
 
+static bool brcmf_sdio_rx_pkt_is_avail(struct brcmf_sdio *bus)
+{
+	struct brcmf_sdio_dev *sdiod = bus->sdiodev;
+	u32 newstatus = 0;
+	u32 intstat_addr = bus->sdio_core->base + SD_REG(intstatus);
+	int err = 0;
+	bool ret = true;
+
+	if (!inf_btsdio_is_active(bus->sdiodev->bus_if))
+		return true;
+
+	/* read interrupt to get fifo status*/
+	newstatus = brcmf_sdiod_readl(sdiod, intstat_addr, &err);
+	if (err < 0) {
+		brcmf_err("read int status failed %d\n", err);
+		ret = false;
+	} else if (!(newstatus & I_XMTDATA_AVAIL)) {
+		/* no more frames */
+		brcmf_dbg(DATA, "no more frames, int status: 0x%08x\n", newstatus);
+		ret = false;
+	}
+
+	return ret;
+}
+
 static uint brcmf_sdio_readframes(struct brcmf_sdio *bus, uint maxframes)
 {
 	struct sk_buff *pkt;		/* Packet for event or data frames */
@@ -2210,9 +2235,16 @@ static uint brcmf_sdio_readframes(struct brcmf_sdio *bus, uint maxframes)
 			continue;
 		}
 
+		sdio_claim_host(bus->sdiodev->func1);
+		/* make sure rx pkt check and rece buf in the same critical section */
+		if (!brcmf_sdio_rx_pkt_is_avail(bus)) {
+			bus->rxpending = false;
+			sdio_release_host(bus->sdiodev->func1);
+			break;
+		}
+
 		rd->len_left = rd->len;
 		/* read header first for unknown frame length */
-		sdio_claim_host(bus->sdiodev->func1);
 		if (!rd->len) {
 			ret = brcmf_sdiod_recv_buf(bus->sdiodev, SDIO_FUNC_2,
 						   bus->rxhdr, BRCMF_FIRSTREAD);