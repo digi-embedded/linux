Infineon/ifx-wireless-drivers

non-upstream: btsdio: reset bt in remove flow when bt shared sdio enabled
When BT over SDIO, need to follow below sequence to insert/remove
wlan/bt module:
* insert wlan -> insert bt -> remove bt -> remove wlan

So wlan driver must reset bt in remove flow, otherwise sdio core will
not be reset properly.

* 43022: set BT reset by BT driver
* H1 combo: set BT reset by WL driver

Issue history
* commit 6c5848fb21b7 ("non-upstream: reset bt in remove flow when
bt shared sdio enabled")
* commit 521c17c33268 ("non-upstream: set bt reset on H1 only")
* commit 0f1a4c85e942 ("non-upstream: remove invalid register access
to fix command error")

Fixes CYW55500A0-453, SWWLAN-146104

Signed-off-by: Owen Huang <Owen.Huang@infineon.com>

drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.c
@@ -5556,6 +5556,11 @@ void brcmf_sdio_remove(struct brcmf_sdio *bus)
 								       &err);
 					reg_val |= SDIO_CCCR_BRCM_CARDCTRL_WLANRESET;
 
+					if (ifx_btsdio_set_bt_reset(bus_if))
+						reg_val |= SDIO_CCCR_BRCM_CARDCTRL_BTRESET;
+
+					brcmf_dbg(SDIO, "Write CARDCTRL = 0x%x\n", reg_val);
+
 					brcmf_sdiod_func0_wb(sdiodev, SDIO_CCCR_BRCM_CARDCTRL,
 							     reg_val, &err);
 					brcmf_sdiod_func0_wb(sdiodev, SDIO_CCCR_ABORT,

digi-embedded/linux

drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.c
@@ -5573,6 +5573,11 @@ void brcmf_sdio_remove(struct brcmf_sdio *bus)
 								       &err);
 					reg_val |= SDIO_CCCR_BRCM_CARDCTRL_WLANRESET;
 
+					if (inf_btsdio_set_bt_reset(bus_if))
+						reg_val |= SDIO_CCCR_BRCM_CARDCTRL_BTRESET;
+
+					brcmf_dbg(SDIO, "Write CARDCTRL = 0x%x\n", reg_val);
+
 					brcmf_sdiod_func0_wb(sdiodev, SDIO_CCCR_BRCM_CARDCTRL,
 							     reg_val, &err);
 					brcmf_sdiod_func0_wb(sdiodev, SDIO_CCCR_ABORT,