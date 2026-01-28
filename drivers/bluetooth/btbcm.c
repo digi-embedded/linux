ab87d1b6b2d871ea88ce915faafc1dcdf28d6542
Hector Palacios <hector.palacios@digi.com>
2026-01-28 12:58:15 +0100

    bluetooth: btbcm: Add CYW55560A1 variant
    This commit adds support to CYW55571 chipset.
    
    Signed-off-by: Hector Palacios <hector.palacios@digi.com>

--- drivers/bluetooth/btbcm.c
+++ drivers/bluetooth/btbcm.c
@@ -520,6 +520,7 @@ static const struct bcm_subver_table bcm_uart_subver_table[] = {
 	{ 0x410c, "BCM43430B0"	},	/* 002.001.012 */
 	{ 0x2119, "BCM4373A0"	},	/* 001.001.025 */
 	{ 0x2220, "CYW55500A1"	},	/* 001.002.032 */
+	{ 0x2257, "CYW55560A1"  },	/* 001.002.087 */
 	{ }
 };
 
